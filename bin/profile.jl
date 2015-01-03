23a0b28eb26ae2956f5aed4ca86074ca00c347fa
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-01-03 11:32:44 -0800

    fixed profiling script

--- bin/profile.jl
+++ bin/profile.jl
@@ -1,18 +1,11 @@
 #!/usr/bin/env julia
 
-import StampBlob
-import Planck
-import ElboDeriv
-import OptimizeElbo
-import ViInit
-
-using Synthetic
-using CelesteTypes
+using Celeste
 
 
 function small_image_profile()
 	srand(1)
-	blob0 = StampBlob.load_stamp_blob("164.4311-39.0359")
+	blob0 = StampBlob.load_stamp_blob(ENV["STAMP"], "164.4311-39.0359")
 	for b in 1:5
 		blob0[b].H, blob0[b].W = 100, 200
 	end
@@ -20,12 +13,12 @@ function small_image_profile()
 	brightness7000K = real(Planck.photons_expected(7000., 10., 1e4))
 
 	three_bodies = [
-		StarParams([10.1, 12.2], brightness7000K),
-		GalaxyParams([71.3, 100.4], brightness7000K , 0.1, [6, 0., 6.]),
-		GalaxyParams([81.5, 103.6], brightness7000K , 0.1, [6, 0., 6.]),
+		Synthetic.StarParams([10.1, 12.2], brightness7000K),
+		Synthetic.GalaxyParams([71.3, 100.4], brightness7000K , 0.1, [6, 0., 6.]),
+		Synthetic.GalaxyParams([81.5, 103.6], brightness7000K , 0.1, [6, 0., 6.]),
 	]
 
-   	blob = gen_blob(blob0, three_bodies)
+   	blob = Synthetic.gen_blob(blob0, three_bodies)
 	M = ViInit.sample_prior()
 	V = ViInit.init_sources(blob)
 	elbo = ElboDeriv.elbo(blob, M, V)
