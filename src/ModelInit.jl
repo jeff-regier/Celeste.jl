b003b6f427b40a9bbb81a35409d9b244cd230f46
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-01-31 13:24:45 -0800

    keep values of row in [0.0001,.9999]

--- src/ModelInit.jl
+++ src/ModelInit.jl
@@ -70,7 +70,7 @@ function init_source(ce::CatalogEntry)
 
 	ret[ids.theta] = min(max(ce.gal_frac_dev, 0.01), 0.99)
 
-	ret[ids.rho] = ce.gal_ab
+	ret[ids.rho] = min(max(ce.gal_ab, .9999), 0.0001)
 	ret[ids.phi] = ce.gal_angle
 	ret[ids.sigma] = max(0.2, ce.gal_scale)
 
