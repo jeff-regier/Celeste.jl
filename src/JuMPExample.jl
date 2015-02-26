68ee66a54a33706e43c755230f5f37dad85043e1
rgiordan <rgiordan@gmail.com>
2015-02-25 22:15:38 -0800

    I think this wont work without ipopt

--- src/JuMPExample.jl
+++ src/JuMPExample.jl
@@ -89,3 +89,33 @@ println("x = ", getValue(x))
 println("aux = ", getValue(aux))
 println("y = ", getValue(y))
 
+
+
+
+###########
+# An example from the JuMP web site:
+N     = 1000
+ni    = N
+h     = 1/ni
+alpha = 350
+
+# So this doesn't work.  I suspect problems fith NLopt.
+m = Model(solver=NLoptSolver(algorithm=:GN_ISRES))
+
+@defVar(m, -1 <= t[1:(ni+1)] <= 1)
+@defVar(m, -0.05 <= x[1:(ni+1)] <= 0.05)
+@defVar(m, u[1:(ni+1)])
+
+@setNLObjective(m, Min, sum{ 0.5*h*(u[i+1]^2 + u[i]^2) + 0.5*alpha*h*(cos(t[i+1]) + cos(t[i])), i = 1:ni})
+
+# cons1
+for i in 1:ni
+    @addNLConstraint(m, x[i+1] - x[i] - (0.5h)*(sin(t[i+1])+sin(t[i])) == 0)
+end
+# cons2
+for i in 1:ni
+    @addConstraint(m, t[i+1] - t[i] - (0.5h)*u[i+1] - (0.5h)*u[i] == 0)
+end
+
+solve(m)
+
