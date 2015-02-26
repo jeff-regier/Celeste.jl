da56726832478ed16251de0d6e88188b802b9a12
rgiordan <rgiordan@gmail.com>
2015-02-25 17:36:05 -0800

    Linear arrays of experssions

--- src/JuMPExample.jl
+++ src/JuMPExample.jl
@@ -1,5 +1,66 @@
 using JuMP
 using NLopt
+using Clp
+
+
+#################
+# A stupid but more complicated example.
+
+
+
+n = 100
+k = 3
+x = randn(n, k)
+true_beta = randn(k)
+y = x * true_beta + randn(n)
+
+# This is stupid but is working with sums and array expressions.
+m = Model(solver=ClpSolver())
+@defVar(m, -10 <= beta[1:k] <= 10)
+@defExpr(xb[i=1:n], dot(vec(x[i,:]), beta))
+@defExpr(eps[i=1:n], y[i] - xb[i])
+@setObjective(m, Min, sum(xb))
+status = solve(m)
+getObjectiveValue(m)
+
+
+
+
+n = 100
+k = 3
+x = randn(n, k)
+true_beta = randn(k)
+y = x * true_beta + randn(n)
+
+
+m = Model(solver=NLoptSolver(algorithm=:LD_LBFGS))
+@defVar(m, -10 <= gamma <= 10)
+index = 2
+@defNLExpr(eps[i=1:n], gamma)
+@defNLExpr(eps_sum, sum{eps})
+@setNLObjective(m, Min, eps_sum)
+status = solve(m)
+getObjectiveValue(m)
+
+
+@setNLObjective(m, Min, sum{eps, i=1:n})
+
+@defVar(m, -10 <= beta[1:k] <= 10)
+
+cost = [1, 2, 3]
+index = 2
+
+#@defNLExpr(error2[i=1:k], beta[i])
+
+#for this_row in 1:n 
+#  @defNLExpr(error2[i=this_row], (y - beta[1] * x[this_row, 1] - beta[2] * x[this_row, 2] - beta[3] * x[this_row, 3])^2)
+#end
+#@defNLExpr(error2[1], (y - beta[1] * x[1, 1] - beta[2] * x[1, 2] - beta[3] * x[1, 3])^2)
+
+
+
+#################
+# A basic working example
 
 function logit(x)
     exp(x) / (1 + exp(x))
@@ -12,13 +73,16 @@ m = Model(solver=NLoptSolver(algorithm=:LD_LBFGS))
 
 # This works:
 @defNLExpr(aux, 2 * exp(x) / (1 + exp(x)))
+@defNLExpr(sin_x, sin(x))
 
 # This does not work:
 #@defNLExpr(aux, 2 * logit(x))
 
 # This works:
 @defNLExpr(aux2, aux^2)
-@setNLObjective(m, Max, 5aux2 + 3y)
+
+
+@setNLObjective(m, Max, 5aux2 + 3y + sin_x)
 print(m)
 status = solve(m)
 
@@ -28,10 +92,10 @@ println("y = ", getValue(y))
 
 
 
-
-
-
-
+#################
+# Here is a (bad) way to do it with nonlinear equality constraints.
+# This is the only NLopt solver that accomodates nonlinear equality constriants.
+m = Model(solver=NLoptSolver(algorithm=:GN_ISRES))
 
 # Every variable must be bounded for GN_ISRES.
 @defVar(m, -1e6 <= x <= 1e6)
