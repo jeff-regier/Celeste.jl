d73f6cb8d37a3d3025354cac80064ba4e3a7698b
Ryan Giordano <rgiordan@gmail.com>
2015-02-27 22:56:37 -0800

    Simple ELBO in JuMP
    Former-commit-id: 8a092bb2f7dea88dcd4062525ab68871442f1edc

--- src/JuMPExamples/JuMPGammaPoisson.jl
+++ src/JuMPExamples/JuMPGammaPoisson.jl
@@ -26,4 +26,41 @@ phi_ns = [convert(Float64, pdf(Normal(0, 0.06), m_i - mu_s)) for m_i in m, mu_s 
 x = [rand(Poisson(b)) for b in phi_ns * true_brightness]
 
 
+# Optimize with JuMP
+m = Model()
+@defVar(m, gamma[1:S, 1:2] >= 0)
+@defVar(m, zeta[1:S, 1:2] >= 0)
+@defVar(m, 0 <= chi[1:S] <= 1)
+
+# Define the r expectations.
+@defNLExpr(e_ra[s=1:S, a=1:2],
+	       gamma[s, a] * zeta[s, a])
+@defNLExpr(e_ra2[s=1:S, a=1:2],
+	       (1 + gamma[s, a]) * gamma[s, a] * (zeta[s, a] ^ 2))
+@defNLExpr(e_r[s=1:S],
+	       chi[s] * e_ra[s, 2] + (1 - chi[s]) * e_ra[s, 2])
+@defNLExpr(var_r[s=1:S],
+	       chi[s] * e_ra2[s, 1] + (1 - chi[s]) * e_ra2[s, 2] - (e_r[s]) ^ 2)
+
+# Define the F expectations.
+@defNLExpr(e_fns[n=1:N, s=1:S], e_r[s] * phi_ns[n, s])
+@defNLExpr(var_fns[n=1:N, s=1:S], var_r[s] * (phi_ns[n, s])^2)
+@defNLExpr(e_fn[n=1:N], sum{e_fns[n, s], s=1:S})
+@defNLExpr(var_fn[n=1:N], sum{var_fns[n, s], s=1:S})
+@defNLExpr(e_log_fn[n=1:N], log(e_fn[n] ^ 2) - var_fn[n] / (2 * e_fn[n] ^ 2))
+@defNLExpr(e_log_lik, sum{x[n] * e_log_fn[n] - e_fn[n], n=1:N})
+
+# Define the entropy.
+@defNLExpr(ent_rsa[s=1:S, a=1:2],
+	       gamma[s, a] + log(zeta[s, a]) + lgamma(zeta[s, a]))
+@defNLExpr(ent_as[s=1:S],
+	       -1 * chi[s] * log(chi[s]) - (1 - chi[s]) * log(1 - chi[s]))
+@defNLExpr(entropy, sum{ent_rsa[s, a], s=1:S, a=1:2} + sum{ent_as[s], s=1:S})
+
+@setNLObjective(m, Min, entropy + e_log_lik)
+solve(m)
+
+println("alpha = ", getValue(alpha))
+return getObjectiveValue(m)
+
 
