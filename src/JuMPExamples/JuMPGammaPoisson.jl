ed3c2eed34dbf44e38b6dce1537125d5375d2528
Ryan Giordano <rgiordan@gmail.com>
2015-02-28 13:45:22 -0800

    Entropy working with Jeffs fix
    Former-commit-id: 56e4d194c45eb68dc1699997b3584ca53225b404

--- src/JuMPExamples/JuMPGammaPoisson.jl
+++ src/JuMPExamples/JuMPGammaPoisson.jl
@@ -2,7 +2,7 @@ using JuMP
 using Distributions
 #using Gadfly
 
-N = 5000
+N = 500
 S = 3
 
 #################################
@@ -35,16 +35,16 @@ m = Model()
 @defVar(m, log_zeta[1:S, 1:2])
 @defVar(m, logit_chi[1:S])
 
-# Unconstrin the variables
-@defNLExpr(chi[s=1:S], exp(logit_chi[s]) / (1 + exp(logit_chi[s])))
-@defNLExpr(gamma[s=1:S, a=1:2], exp(log_gamma[s, a]))
-@defNLExpr(zeta[s=1:S, a=1:2], exp(log_zeta[s, a]))
+# Unconstrain the variables.  Does this really help at all?
+@defNLExpr(q_chi[s=1:S], exp(logit_chi[s]) / (1 + exp(logit_chi[s])))
+@defNLExpr(q_gamma[s=1:S, a=1:2], exp(log_gamma[s, a]))
+@defNLExpr(q_zeta[s=1:S, a=1:2], exp(log_zeta[s, a]))
 
 # Define the r expectations.
-@defNLExpr(e_ra[s=1:S, a=1:2], gamma[s, a] * zeta[s, a])
-@defNLExpr(e_ra2[s=1:S, a=1:2], (1 + gamma[s, a]) * gamma[s, a] * (zeta[s, a] ^ 2))
-@defNLExpr(e_r[s=1:S], chi[s] * e_ra[s, 1] + (1 - chi[s]) * e_ra[s, 2])
-@defNLExpr(var_r[s=1:S], chi[s] * e_ra2[s, 1] + (1 - chi[s]) * e_ra2[s, 2] - (e_r[s]) ^ 2)
+@defNLExpr(e_ra[s=1:S, a=1:2], q_gamma[s, a] * q_zeta[s, a])
+@defNLExpr(e_ra2[s=1:S, a=1:2], (1 + q_gamma[s, a]) * q_gamma[s, a] * (q_zeta[s, a] ^ 2))
+@defNLExpr(e_r[s=1:S], q_chi[s] * e_ra[s, 1] + (1 - q_chi[s]) * e_ra[s, 2])
+@defNLExpr(var_r[s=1:S], q_chi[s] * e_ra2[s, 1] + (1 - q_chi[s]) * e_ra2[s, 2] - (e_r[s]) ^ 2)
 
 # Define the F expectations.
 @defNLExpr(e_fns[n=1:N, s=1:S], e_r[s] * phi_ns[n, s])
@@ -56,25 +56,16 @@ m = Model()
 
 # Define the entropy.
 @defNLExpr(ent_rsa[s=1:S, a=1:2],
-	       gamma[s, a] + log(zeta[s, a]) + lgamma(zeta[s, a]))
+	       q_gamma[s, a] + log(q_zeta[s, a]) + lgamma(q_zeta[s, a]) +
+	       (1 - q_zeta[s, a]) * digamma(q_zeta[s, a]))
 @defNLExpr(ent_as[s=1:S],
-	       -1 * chi[s] * log(chi[s]) - (1 - chi[s]) * log(1 - chi[s]))
+	       -1 * q_chi[s] * log(q_chi[s]) - (1 - q_chi[s]) * log(1 - q_chi[s]))
 @defNLExpr(entropy, sum{ent_rsa[s, a], s=1:S, a=1:2} + sum{ent_as[s], s=1:S})
 
 # TODO: put in the priors, now the difference between a star and galaxy isn't
 # present in the model.
 
 # The entropy is currently not working because of the lgamma function.
-@setNLObjective(m, Max, e_log_lik)
+@setNLObjective(m, Max, e_log_lik + entropy)
 solve(m)
 
-
-
-
-# lgamma doesn't work:
-m = Model()
-@defVar(m, 3.0 <= x <= 4.0)
-@setNLObjective(m, Max, lgamma(x))
-solve(m) # Expect lgamma(4.0)
-# ERROR: InexactError()
-#  in float64 at float.jl:55
