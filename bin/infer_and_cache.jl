c8e475fad484baa285e2b1adefaad3f4fd443941
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-03-04 17:50:02 -0800

    for synthetic data, colors are sampled too now (not just the pixel values)
    Former-commit-id: 07dfca308f2adc11a4cda1288e20aed6be0ebe57

--- bin/infer_and_cache.jl
+++ bin/infer_and_cache.jl
@@ -4,6 +4,7 @@
 
 using Celeste
 using CelesteTypes
+import Synthetic
 
 
 function peak_infer_and_cache(stamp_id)
@@ -31,7 +32,40 @@ function cat_infer_and_cache(stamp_id)
 end
 
 
-if length(ARGS) > 0
-	cat_infer_and_cache(ARGS[1])
+function synth_infer_and_cache(stamp_id)
+    bright(ce) = sum(ce.star_fluxes) > 3 || sum(ce.gal_fluxes) > 3
+    inbounds(ce) = ce.pos[1] > -10. && ce.pos[2] > -10 &&
+        ce.pos[1] < 61 && ce.pos[2] < 61
+
+    f = open(ENV["STAMP"]"/cat-synth-$stamp_id.dat")
+    cat_synth = deserialize(f)
+    close(f)
+
+    blob0 = SDSS.load_stamp_blob(ENV["STAMP"], stamp_id)
+    blob = Synthetic.gen_blob(blob0, cat_synth)
+
+    cat_synth = filter(bright, cat_synth)
+    cat_synth = filter(inbounds, cat_synth)
+
+    cat_primary = SDSS.load_stamp_catalog(ENV["STAMP"], stamp_id, blob, match_blob=true)
+    cat_primary = filter(bright, cat_primary)
+    cat_primary = filter(inbounds, cat_primary)
+
+    mp = ModelInit.cat_init(cat_primary)
+
+    OptimizeElbo.maximize_elbo(blob, mp)
+
+    f = open(ENV["MODEL"]"/S-$stamp_id.dat", "w+")
+    serialize(f, mp)
+    close(f)
+end
+
+
+if length(ARGS) == 2
+    if ARGS[1] == "V"
+        cat_infer_and_cache(ARGS[2])
+    elseif ARGS[1] == "S"
+        synth_infer_and_cache(ARGS[2])
+    end
 end
 
