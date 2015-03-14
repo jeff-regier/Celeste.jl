b96ce637819a3ebb4c11b0aed7d30d8a908bdec9
Ryan Giordano <rgiordan@gmail.com>
2015-03-14 16:55:29 -0700

    cleaning up the tests

--- src/TestElboJuMP.jl
+++ src/TestElboJuMP.jl
@@ -1,190 +1,143 @@
+using Dates
+using Base.Test
+
 # Pull out the tests from ElboJuMP.jl
 
 ############################
 # Test the brightness
-
-jump_e_l_a = [ ReverseDiffSparse.getvalue(E_l_a[s, b, a], celeste_m.colVal)
-               for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
+jump_e_l_a = Float64[ ReverseDiffSparse.getvalue(E_l_a[s, b, a], celeste_m.colVal)
+                      for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
 
 celeste_e_l_a = [ ElboDeriv.SourceBrightness(mp.vp[s]).E_l_a[b, a].v
                   for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
 
-jump_e_ll_a = [ ReverseDiffSparse.getvalue(E_ll_a[s, b, a], celeste_m.colVal)
-               for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
+jump_e_ll_a = Float64[ ReverseDiffSparse.getvalue(E_ll_a[s, b, a], celeste_m.colVal)
+                       for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
 
 celeste_e_ll_a = [ ElboDeriv.SourceBrightness(mp.vp[s]).E_ll_a[b, a].v
                   for s=1:mp.S, a=1:CelesteTypes.I, b=1:CelesteTypes.B ]
 
-jump_e_ll_a - celeste_e_ll_a
+@test_approx_eq jump_e_l_a celeste_e_l_a
+@test_approx_eq jump_e_ll_a celeste_e_ll_a
 
 ############################
 # Test the bivariate normal functions
 
-star_mcs, gal_mcs = ElboDeriv.load_bvn_mixtures(blobs[1].psf, mp)
+star_mcs, gal_mcs = ElboDeriv.load_bvn_mixtures(blobs[1].psf, mp);
 
 ### Check the stars:
 celeste_star_mean = [
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[1][k, s].the_mean[row]
-    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2 ]
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2 ];
 jump_star_mean =
 	[ ReverseDiffSparse.getvalue(star_mean[b, s, k, row],
    							  celeste_m.colVal)
-       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2 ]
+       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2 ];
+@test_approx_eq celeste_star_mean jump_star_mean
 
 celeste_star_precision = [
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[1][k, s].precision[row, col]
-    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2, col=1:2 ]
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2, col=1:2 ];
 jump_star_precision =
 	[ ReverseDiffSparse.getvalue(star_precision[b, s, k, row, col],
    							     celeste_m.colVal)
-       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2, col=1:2 ]
+       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, row=1:2, col=1:2 ];
 
-celeste_star_precision - jump_star_precision
+@test_approx_eq celeste_star_precision jump_star_precision
 
 celeste_star_z = [
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[1][k, s].z
-    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp ]
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp ];
 jump_star_z =
 	[ ReverseDiffSparse.getvalue(star_z[b, s, k],
    							     celeste_m.colVal)
-       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp ]
+       for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp ];
+@test_approx_eq celeste_star_z jump_star_z
 
 
-# This is super slow, so just look at a few indices
-# jump_galaxy_type1_precision =
-# 	[ ReverseDiffSparse.getvalue(galaxy_type1_precision[b, s, k, g_k, row, col],
-#    							     celeste_m.colVal)
-#        for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
-#        row=1:2, col=1:2 ]
-b = 2
-s = 2
-k = 2
-g_k = 4
 
-### Check the galaxies:
-celeste_galaxy_type1_precision = [
+
+############
+# Test the galaxies
+
+jump_galaxy_type1_precision = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type1_precision[b, s, k, g_k, row, col],
+  						       celeste_m.colVal)
+		for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
+		row=1:2, col=1:2 ];
+
+celeste_galaxy_type1_precision = Float64[
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[2][k, g_k, 1, s].bmc.precision[row, col]
     for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
-    row=1:2, col=1:2 ]
+    row=1:2, col=1:2 ];
 
- celeste_galaxy_type2_precision = [
+@test_approx_eq celeste_galaxy_type1_precision jump_galaxy_type1_precision
+
+
+jump_galaxy_type2_precision = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type2_precision[b, s, k, g_k, row, col],
+  						       celeste_m.colVal)
+		for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp,
+		row=1:2, col=1:2 ];
+
+celeste_galaxy_type2_precision = Float64[
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[2][k, g_k, 2, s].bmc.precision[row, col]
     for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp,
-    row=1:2, col=1:2 ]
+    row=1:2, col=1:2 ];
 
-celeste_galaxy_type1_z = [
+@test_approx_eq celeste_galaxy_type2_precision jump_galaxy_type2_precision
+
+
+jump_galaxy_type1_z = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type1_z[b, s, k, g_k],
+			    			   celeste_m.colVal)
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp ];
+
+celeste_galaxy_type1_z = Float64[
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[2][k, g_k, 1, s].bmc.z
-    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp ]
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp ];
 
-celeste_galaxy_type2_z = [
+@test_approx_eq celeste_galaxy_type1_z jump_galaxy_type1_z
+
+jump_galaxy_type2_z = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type2_z[b, s, k, g_k],
+			    			   celeste_m.colVal)
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp ];
+
+celeste_galaxy_type2_z = Float64[
     ElboDeriv.load_bvn_mixtures(blobs[b].psf, mp)[2][k, g_k, 2, s].bmc.z
-    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp ]
+    for b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp ];
+
+@test_approx_eq celeste_galaxy_type2_z jump_galaxy_type2_z
 
 
-# Get a var_s from the original code:
-rho = mp.vp[s][ids.rho]
-phi = mp.vp[s][ids.phi]
-sigma = mp.vp[s][ids.sigma]
-pc = blobs[b].psf[k]
-gc = galaxy_prototypes[1][g_k]
-XiXi = Util.get_bvn_cov(rho, phi, sigma)
-mean_s = [0 0]
-var_s = pc.SigmaBar + gc.sigmaTilde * XiXi
-weight = pc.alphaBar * gc.alphaTilde  # excludes theta
+# For deeper debugging, get a var_s from the original code:
+# rho = mp.vp[s][ids.rho]
+# phi = mp.vp[s][ids.phi]
+# sigma = mp.vp[s][ids.sigma]
+# pc = blobs[b].psf[k]
+# gc = galaxy_prototypes[1][g_k]
+# XiXi = Util.get_bvn_cov(rho, phi, sigma)
+# mean_s = [0 0]
+# var_s = pc.SigmaBar + gc.sigmaTilde * XiXi
+# weight = pc.alphaBar * gc.alphaTilde  # excludes theta
 
-jump_xixi = 
-	[ ReverseDiffSparse.getvalue(galaxy_xixi_mat[s, row, col],
-   							     celeste_m.colVal)
-       for row=1:2, col=1:2 ];
+# jump_xixi = 
+# 	[ ReverseDiffSparse.getvalue(galaxy_xixi_mat[s, row, col],
+#    							     celeste_m.colVal)
+#        for row=1:2, col=1:2 ];
 
-jump_var_s = 
-	[ ReverseDiffSparse.getvalue(galaxy_type1_var_s[b, s, k, g_k, row, col],
-   							     celeste_m.colVal)
-       for row=1:2, col=1:2 ];
-jump_var_s - var_s
+# jump_var_s = 
+# 	[ ReverseDiffSparse.getvalue(galaxy_type1_var_s[b, s, k, g_k, row, col],
+#    							     celeste_m.colVal)
+#        for row=1:2, col=1:2 ];
+# jump_var_s - var_s
 
-jump_galaxy_type1_det =
-	ReverseDiffSparse.getvalue(galaxy_type1_det[b, s, k, g_k],
-   							     celeste_m.colVal)
-det(var_s) - jump_galaxy_type1_det
+# jump_galaxy_type1_det =
+# 	ReverseDiffSparse.getvalue(galaxy_type1_det[b, s, k, g_k],
+#    							     celeste_m.colVal)
+# det(var_s) - jump_galaxy_type1_det
 
-# This is super slow for some reason.
-jump_galaxy_type1_precision =
-	[ ReverseDiffSparse.getvalue(galaxy_type1_precision[b, s, k, g_k, row, col],
-   							     celeste_m.colVal)
-       for row=1:2, col=1:2 ]
-
-this_celeste_galaxy_type1_precision =
-   [ celeste_galaxy_type1_precision[b, s, k, g_k, row, col] for row=1:2, col=1:2 ]
-
-jump_galaxy_type1_precision - this_celeste_galaxy_type1_precision
-
-# z
-jump_galaxy_type1_z = ReverseDiffSparse.getvalue(galaxy_type1_z[b, s, k, g_k],
-   							    				 celeste_m.colVal)
-jump_galaxy_type1_z - celeste_galaxy_type1_z[b, s, k, g_k]
-
-
-#########
-# Type 2:
-rho = mp.vp[s][ids.rho]
-phi = mp.vp[s][ids.phi]
-sigma = mp.vp[s][ids.sigma]
-pc = blobs[b].psf[k]
-gc = galaxy_prototypes[2][g_k]
-XiXi = Util.get_bvn_cov(rho, phi, sigma)
-mean_s = [0 0]
-var_s = pc.SigmaBar + gc.sigmaTilde * XiXi
-weight = pc.alphaBar * gc.alphaTilde  # excludes theta
-
-jump_var_s = 
-	[ ReverseDiffSparse.getvalue(galaxy_type2_var_s[b, s, k, g_k, row, col],
-   							     celeste_m.colVal)
-       for row=1:2, col=1:2 ];
-jump_var_s - var_s
-
-jump_galaxy_type2_det =
-	ReverseDiffSparse.getvalue(galaxy_type2_det[b, s, k, g_k],
-   							     celeste_m.colVal)
-
-det(var_s) - jump_galaxy_type2_det
-
-# This is super slow for some reason.
-jump_galaxy_type2_precision =
-	[ ReverseDiffSparse.getvalue(galaxy_type2_precision[b, s, k, g_k, row, col],
-   							     celeste_m.colVal)
-       for row=1:2, col=1:2 ]
-
-this_celeste_galaxy_type2_precision =
-   [ celeste_galaxy_type2_precision[b, s, k, g_k, row, col] for row=1:2, col=1:2 ]
-
-jump_galaxy_type2_precision - this_celeste_galaxy_type2_precision
-
-# z
-jump_galaxy_type2_z = ReverseDiffSparse.getvalue(galaxy_type2_z[b, s, k, g_k],
-   							    				 celeste_m.colVal)
-jump_galaxy_type2_z - celeste_galaxy_type2_z[b, s, k, g_k]
-
-
-# Document the bad determinant.  Doesn't seem bad anymore.
-
-ReverseDiffSparse.getvalue(galaxy_type1_det[b, s, k, g_k], celeste_m.colVal)
-ReverseDiffSparse.getvalue(galaxy_type1_det_bad[b, s, k, g_k], celeste_m.colVal)
-
-
-# It's prohibitively slow to check each galaxy component, but you
-# can check the sums:
-
-@defNLExpr(foo, sum{galaxy_type1_precision[b, s, k, g_k, row, col],
-	                b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
-	                row=1:2, col=1:2});
-ReverseDiffSparse.getvalue(foo, celeste_m.colVal) - sum(celeste_galaxy_type1_precision)
-
-
-@defNLExpr(foo, sum{galaxy_type2_precision[b, s, k, g_k, row, col],
-	                b=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp,
-	                row=1:2, col=1:2});
-ReverseDiffSparse.getvalue(foo, celeste_m.colVal) - sum(celeste_galaxy_type2_precision)
 
 ############################
 # Test the galaxy and star functions
@@ -220,31 +173,26 @@ for img=1:CelesteTypes.B
     end
 end
 
+jump_star_pdf_f = Float64[
+	ReverseDiffSparse.getvalue(star_pdf_f[img, s, k, pw, ph], celeste_m.colVal) 
+	for img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp,
+	           pw=1:img_w, ph=1:img_h ];
 
-star_celeste_sum = sum(celeste_star_pdf_f)
-gal1_celeste_sum = sum(celeste_gal1_pdf_f)
-gal2_celeste_sum = sum(celeste_gal2_pdf_f)
+jump_gal1_pdf_f = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type1_pdf_f[img, s, k, g_k, pw, ph], celeste_m.colVal) 
+	for img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
+	           pw=1:img_w, ph=1:img_h ];
+
+jump_gal2_pdf_f = Float64[
+	ReverseDiffSparse.getvalue(galaxy_type2_pdf_f[img, s, k, g_k, pw, ph], celeste_m.colVal) 
+	for img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp,
+	           pw=1:img_w, ph=1:img_h ];
+
+@test_approx_eq celeste_star_pdf_f jump_star_pdf_f
+@test_approx_eq celeste_gal1_pdf_f jump_gal1_pdf_f
+@test_approx_eq celeste_gal2_pdf_f jump_gal2_pdf_f
 
 
-# Get the JuMP sum:
-@defNLExpr(sum_star_pdf_f,
-	       sum{star_pdf_f[img, s, k, pw, ph],
-	           img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp,
-	           pw=1:img_w, ph=1:img_h});
-@defNLExpr(sum_gal1_pdf_f,
-	       sum{galaxy_type1_pdf_f[img, s, k, g_k, pw, ph],
-	           img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal1_comp,
-	           pw=1:img_w, ph=1:img_h});
-@defNLExpr(sum_gal2_pdf_f,
-	       sum{galaxy_type2_pdf_f[img, s, k, g_k, pw, ph],
-	           img=1:CelesteTypes.B, s=1:mp.S, k=1:n_pcf_comp, g_k=1:n_gal2_comp,
-	           pw=1:img_w, ph=1:img_h});
-
-star_jump_sum = ReverseDiffSparse.getvalue(sum_star_pdf_f, celeste_m.colVal)
-
-# This is incredibly slow, taking many minutes each to compute, so I'm commenting it out.
-#gal1_jump_sum = ReverseDiffSparse.getvalue(sum_gal1_pdf_f, celeste_m.colVal)
-#gal2_jump_sum = ReverseDiffSparse.getvalue(sum_gal2_pdf_f, celeste_m.colVal)
 
 
 #############################
