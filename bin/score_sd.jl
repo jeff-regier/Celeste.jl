5554db1ee9877bd2fd8817de57b0b9daf107b00e
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-03-24 16:00:52 -0700

    matched pairs significance testing; revised UQ code
    Former-commit-id: 0375b177b0e4be36ed4785deef37994d09479420

--- bin/score_sd.jl
+++ bin/score_sd.jl
@@ -9,26 +9,6 @@ coadd_df = readtable("coadd.csv")
 celeste_err = get_err_df(coadd_df, celeste_df)
 
 
-
-### color UQ
-
-for c in 1:4
-    color_col = symbol("color_$(color_names[c])")
-    color_sd_col = symbol("color_$(color_names[c])_sd")
-
-    non_triv = celeste_df[color_sd_col] .>= 0.0002;
-
-    @printf("%s & %.2f (%.2f) & %d & %.2f (%.2f) & %d \\\\\n",
-        string(color_col),
-        mean(celeste_err[!non_triv, color_col]),
-        std(celeste_err[!non_triv, color_col]) / sqrt(sum(!non_triv)),
-        sum(!non_triv),
-        mean(celeste_err[non_triv, color_col]),
-        std(celeste_err[non_triv, color_col]) / sqrt(sum(non_triv)),
-        sum(non_triv))
-end
-
-
 ### is_star UQ
 
 non_triv = 0.011 .< celeste_df[:is_star] .< .989
@@ -38,14 +18,46 @@ wrong_in_non_triv = sum(celeste_err[non_triv, :missed_stars]) +
 wrong_in_triv = sum(celeste_err[!non_triv, :missed_stars]) + 
         sum(celeste_err[!non_triv, :missed_gals])
 
-@printf(" %d / %d    %d / %d\n",
-    wrong_in_triv,
-    sum(!non_triv),
+@printf("uncertain: %d / %d\ncertain: %d / %d\n\n",
     wrong_in_non_triv,
-    sum(non_triv))
+    sum(non_triv),
+    wrong_in_triv,
+    sum(!non_triv))
 
 
 ### brightness
 
-println("r-r_err cor: ", cor(celeste_df[:flux_r], celeste_err[:flux_r]))
-println("r_sd-r_err cor: ", cor(celeste_df[:flux_r_sd], celeste_err[:flux_r]))
+true_gal = coadd_df[:is_star] .< 0.5
+flux_sd = celeste_df[:star_flux_r_sd]
+flux_sd[true_gal] = celeste_df[true_gal, :gal_flux_r_sd]
+qqs = quantile(flux_sd)
+
+for i in 1:4
+    pop = qqs[i] .<= flux_sd .<= qqs[i + 1]
+    @printf("r_s quartile error: %.2f (%.2f)\n", 
+        mean(celeste_err[pop, :flux_r]),
+        std(celeste_err[pop, :flux_r]) / sqrt(sum(pop)))
+end
+println()
+
+
+### color UQ
+
+for c in 1:4
+    color_col = symbol("color_$(color_names[c])")
+    color_sd_col = symbol("star_color_$(color_names[c])_sd")
+
+    color_sd = celeste_df[symbol("star_color_$(color_names[c])_sd")]
+    color_sd[true_gal] = celeste_df[true_gal, symbol("gal_color_$(color_names[c])_sd")]
+
+    qqs = quantile(color_sd)
+    for i in 1:4
+        pop = qqs[i] .<= color_sd .<= qqs[i + 1]
+        @printf("c_s%d quartile %d error: %.2f (%.2f)\n",
+            c, i,
+            mean(celeste_err[pop, color_col]),
+            std(celeste_err[pop, color_col]) / sqrt(sum(pop)))
+    end
+    println()
+end
+
