4a8657265aeaf90e7ac33686cd431d0b309a90a4
Miles Lubin <miles.lubin@gmail.com>
2015-03-25 22:10:01 -0400

    more simplification
    Former-commit-id: 8e0e444fa6de9084b2ab7204e99a1d450607d74f

--- src/ElboJuMP.jl
+++ src/ElboJuMP.jl
@@ -308,9 +308,9 @@ function build_jump_model(blob::Blob, mp::ModelParams)
     # This is W' * W
     @defNLExpr(galaxy_xixi_mat[s=1:mp.S, row=1:2, col=1:2],
                vp_sigma[s]^2*
-               (sum{ cos(vp_phi[s])^2 + vp_rho[s]^2*sin(vp_phi[s])^2 ; row == 1 && col == 1} +
+               (sum{ 1 + (vp_rho[s]^2-1)*sin(vp_phi[s])^2 ; row == 1 && col == 1} +
                 sum{ (1-vp_rho[s]^2)*sin(2*vp_phi[s])/2; row != col} +
-                sum{ vp_rho[s]^2*cos(vp_phi[s])^2 + sin(vp_phi[s])^2; row == 2 && col == 2}));
+                sum{ 1 + (vp_rho[s]^2-1)*cos(vp_phi[s])^2; row == 2 && col == 2}));
 
     # Terms from GalaxyCacheComponent:
     # var_s and weight for type 1 galaxies:
