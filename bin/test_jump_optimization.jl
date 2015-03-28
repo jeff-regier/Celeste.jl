e39261719599cf3de59cfd68fc9ed900a552ae6a
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-03-28 14:52:56 -0700

    made the size of images in test_jump_optimization.jl non-trivial
    Former-commit-id: 717a246970781aaf1be514d36e49785aac8c954e

--- bin/test_jump_optimization.jl
+++ bin/test_jump_optimization.jl
@@ -11,28 +11,16 @@ using JuMP
 
 
 function test_jump_optimization()
-    # The maximum width and height of the testing image in pixels.
-    # Change these to test sub-images of different sizes.
-    const max_size = 1
-
     # Load some simulated data.  blob contains the image data, and
     # mp is the parameter values.  three_bodies is not used.
     # For now, treat these as global constants accessed within the expressions
     const blob, mp, three_bodies = SampleData.gen_three_body_dataset();
 
-    # Reduce the size of the images for debugging
-    for b in 1:CelesteTypes.B
-        this_height = min(blob[b].H, max_size)
-        this_width = min(blob[b].W, max_size)
-        blob[b].H = this_height
-        blob[b].W = this_width
-        blob[b].pixels = blob[b].pixels[1:this_height, 1:this_width] 
-    end
-
     jump_m, jump_elbo = ElboJuMP.build_jump_model(blob, mp);
 
     @setNLObjective(jump_m, Max, jump_elbo)
-    println("calling `solve` for a $(max_size^2)-pixel image...")
+    num_pixels = sum([img.H * img.W for img in blob])
+    println("calling `solve` for a $(num_pixels)-pixel image...")
     status = solve(jump_m)
     println("Objective value: ", getObjectiveValue(jump_m))
 end
