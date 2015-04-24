63adb72fcfcfee9e07033879cf9a79a4941e4272
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-04-23 22:16:57 -0700

    updated to benchmark.jl to work with the current version of CelesteTypes.jl

--- bin/benchmark.jl
+++ bin/benchmark.jl
@@ -3,27 +3,31 @@
 using Celeste
 using CelesteTypes
 
-import Planck
 import Synthetic
 
 
+const dat_dir = joinpath(Pkg.dir("Celeste"), "dat")
+
+
 function small_image_profile()
-	srand(1)
-	blob0 = SDSS.load_stamp_blob(ENV["STAMP"], "164.4311-39.0359")
-	for b in 1:5
-		blob0[b].H, blob0[b].W = 2000, 2000
-	end
+    srand(1)
 
-	brightness7000K = real(Planck.photons_expected(7000., 10., 1e4))
+    blob0 = SDSS.load_stamp_blob(dat_dir, "164.4311-39.0359")
+    for b in 1:5
+        blob0[b].H, blob0[b].W = 1000, 1000
+    end
 
-	S = 300
-	locations = rand(2, S) .* 2000.
+    fluxes = [4.451805E+03,1.491065E+03,2.264545E+03,2.027004E+03,1.846822E+04]
 
-	S_bodies = CatalogEntry[CatalogStar(locations[:, s][:], brightness7000K) for s in 1:S]
+    S = 100
+    locations = rand(2, S) .* 1000.
 
-   	blob = Synthetic.gen_blob(blob0, S_bodies)
-	mp = ModelInit.cat_init(S_bodies, patch_radius=20., tile_width=10)
-	elbo = ElboDeriv.elbo(blob, mp)
+    S_bodies = CatalogEntry[CatalogEntry(locations[:, s][:], true, 
+        fluxes, fluxes, 0.1, .7, pi/4, 4.) for s in 1:S]
+
+       blob = Synthetic.gen_blob(blob0, S_bodies)
+    mp = ModelInit.cat_init(S_bodies, patch_radius=20., tile_width=10)
+    elbo = ElboDeriv.elbo(blob, mp)
 end
 
 
