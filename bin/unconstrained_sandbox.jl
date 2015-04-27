0d99053a272d91672ec2c79a8766c950c6bb875b
Ryan Giordano <rgiordan@gmail.com>
2015-04-27 10:45:39 -0700

    Systematic comparison for unit tests
    Former-commit-id: 8c95a32a582a227ba925103de07ae08268254a98

--- bin/unconstrained_sandbox.jl
+++ bin/unconstrained_sandbox.jl
@@ -23,9 +23,28 @@ import OptimizeElbo
 import ElboDeriv
 import SampleData
 
+function compare_solutions(mp_rect::ModelParams, mp_free::ModelParams,
+    res_rect_max_f, res_free_max_f, res_rect_iter_count, res_free_iter_count)
+    # Compare the parameters, fits, and iterations.
+    println("===================")
+    println("Differences:")
+    for var_name in names(ids)
+        println(var_name)
+        for s in 1:mp_init.S
+            println(s, ": ", mp_free.vp[s][ids.(var_name)] - mp_rect.vp[s][ids.(var_name)])
+        end
+    end
+    println("===================")
 
-blob, mp_init, body = SampleData.gen_sample_star_dataset();
-#blob, mp_init, body = SampleData.gen_sample_galaxy_dataset();
+    println("Function value (free / rect): ", res_free_max_f / res_rect_max_f)
+    println("Iterations: (free / rect): ", res_free_iter_count / res_rect_iter_count)
+end
+
+########################################
+# Three basic testing problems
+
+#blob, mp_init, body = SampleData.gen_sample_star_dataset();
+blob, mp_init, body = SampleData.gen_sample_galaxy_dataset();
 #blob, mp_init, body = SampleData.gen_three_body_dataset();
 
 mp_original = deepcopy(mp_init)
@@ -48,74 +67,76 @@ res_rect_iter_count, res_rect_max_f, res_rect_max_x, res_rect_ret =
     OptimizeElbo.maximize_f(ElboDeriv.elbo_likelihood, blob, mp_rect, rect_transform,
       	                    lbs_rect, ubs_rect, omitted_ids=omitted_ids, xtol_rel = 1e-7, ftol_abs = 1e-6)
 
-
-ElboDeriv.elbo_likelihood(blob, mp_rect).v
-ElboDeriv.elbo_likelihood(blob, mp_free).v
-
-
-# Compare the parameters, fits, and iterations.
-println("===================")
-println("Differences:")
-for var_name in names(ids)
-	println(var_name)
-	for s in 1:mp_init.S
-		println(s, ": ", mp_free.vp[s][ids.(var_name)] - mp_rect.vp[s][ids.(var_name)])
-	end
-end
-println("===================")
-
-res_free_max_f / res_rect_max_f
-res_free_iter_count / res_rect_iter_count
-
-# blob, mp, body = gen_sample_star_dataset();
-# OptimizeElbo.maximize_likelihood(blob, mp, free_transform, xtol_rel=1e-16, ftol_abs=1e-10)
-# #-529466.9212112223, 248
-# verify_sample_star(mp.vp[1], [10.1, 12.2])
-
-# blob, mp, body = gen_sample_star_dataset();
-# OptimizeElbo.maximize_likelihood(blob, mp, rect_transform, xtol_rel=1e-16, ftol_abs=1e-10)
-# # -529466.9212112254, 262
-# verify_sample_star(mp.vp[1], [10.1, 12.2])
+compare_solutions(mp_rect, mp_free, res_rect_max_f, res_free_max_f, res_rect_iter_count, res_free_iter_count)
 
 
 
+########################################
 # From test_peak_init_2body_optimization
 srand(1)
-blob0 = SDSS.load_stamp_blob(dat_dir, "164.4311-39.0359")
-
+blob0 = SDSS.load_stamp_blob(SampleData.dat_dir, "164.4311-39.0359");
 two_bodies = [
-    sample_ce([11.1, 21.2], true),
-    sample_ce([15.3, 31.4], false),
-]
+    SampleData.sample_ce([11.1, 21.2], true),
+    SampleData.sample_ce([15.3, 31.4], false),
+];
 
-blob = Synthetic.gen_blob(blob0, two_bodies)
-mp = ModelInit.peak_init(blob) #one giant tile, giant patches
-@test mp.S == 2
+blob = Synthetic.gen_blob(blob0, two_bodies);
+mp = ModelInit.peak_init(blob); #one giant tile, giant patches
 
-OptimizeElbo.maximize_likelihood(blob, mp, trans)
+mp_rect_2body = deepcopy(mp)
+res_rect_iter_count_2body, res_rect_max_f_2body, res_rect_max_x, res_rect_ret =
+    OptimizeElbo.maximize_likelihood(blob, mp_rect_2body, rect_transform)
 
-verify_sample_star(mp.vp[1], [11.1, 21.2])
-verify_sample_galaxy(mp.vp[2], [15.3, 31.4])
+mp_free_2body = deepcopy(mp)
+res_free_iter_count_2body, res_free_max_f_2body, res_free_max_x, res_free_ret =
+    OptimizeElbo.maximize_likelihood(blob, mp_free_2body, free_transform)
+
+compare_solutions(mp_rect_2body, mp_free_2body,
+    res_rect_max_f_2body, res_free_max_f_2body,
+    res_rect_iter_count_2body, res_free_iter_count_2body)
 
 
+
+#################################
 # From test_full_elbo_optimization
-blob, mp, body = gen_sample_galaxy_dataset(perturb=true)
-OptimizeElbo.maximize_elbo(blob, mp, trans)
-verify_sample_galaxy(mp.vp[1], [8.5, 9.6])
+
+blob, mp, body = SampleData.gen_sample_galaxy_dataset(perturb=true);
+
+mp_rect_elbo = deepcopy(mp);
+res_rect_iter_count_elbo, res_rect_max_f_elbo, res_rect_max_x, res_rect_ret =
+    OptimizeElbo.maximize_elbo(blob, mp_rect_elbo, rect_transform)
+
+mp_free_elbo = deepcopy(mp);
+res_free_iter_count_elbo, res_free_max_f_elbo, res_free_max_x, res_free_ret =
+    OptimizeElbo.maximize_elbo(blob, mp_free_elbo, free_transform)
+
+compare_solutions(mp_rect_elbo, mp_free_elbo,
+    res_rect_max_f_elbo, res_free_max_f_elbo,
+    res_rect_iter_count_elbo, res_free_iter_count_elbo)
 
 
 
+
+#############################################
 # From test_real_stamp_optimization.  This is slow.
-blob = SDSS.load_stamp_blob(dat_dir, "5.0073-0.0739")
-cat_entries = SDSS.load_stamp_catalog(dat_dir, "s82-5.0073-0.0739", blob)
-bright(ce) = sum(ce.star_fluxes) > 3 || sum(ce.gal_fluxes) > 3
-cat_entries = filter(bright, cat_entries)
+blob = SDSS.load_stamp_blob(dat_dir, "5.0073-0.0739");
+cat_entries = SDSS.load_stamp_catalog(dat_dir, "s82-5.0073-0.0739", blob);
+bright(ce) = sum(ce.star_fluxes) > 3 || sum(ce.gal_fluxes) > 3;
+cat_entries = filter(bright, cat_entries);
 inbounds(ce) = ce.pos[1] > -10. && ce.pos[2] > -10 &&
-    ce.pos[1] < 61 && ce.pos[2] < 61
-cat_entries = filter(inbounds, cat_entries)
+    ce.pos[1] < 61 && ce.pos[2] < 61;
+cat_entries = filter(inbounds, cat_entries);
 
-mp = ModelInit.cat_init(cat_entries)
-OptimizeElbo.maximize_elbo(blob, mp, trans)
+mp = ModelInit.cat_init(cat_entries);
+
+mp_rect = deepcopy(mp);
+res_rect_iter_count, res_rect_max_f, res_rect_max_x, res_rect_ret =
+    OptimizeElbo.maximize_elbo(blob, mp_rect, rect_transform)
+
+mp_free = deepcopy(mp);
+res_free_iter_count, res_free_max_f, res_free_max_x, res_free_ret =
+    OptimizeElbo.maximize_elbo(blob, mp_free, free_transform)
+
 
 
 
