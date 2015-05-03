c093dca3a009803e15af9c901ad6dc4639fdad83
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-05-03 14:42:59 -0700

    removed unnecessary imports from SampleData.jl
    Former-commit-id: b200e32787e253b52545b35c09fd5441be131c2c

--- src/SampleData.jl
+++ src/SampleData.jl
@@ -1,15 +1,13 @@
-
 module SampleData
-
-using CelesteTypes
-import SDSS
-import OptimizeElbo
-import ModelInit
 
 using Base.Test
 using Distributions
+using CelesteTypes
+
+import SDSS
+import ModelInit
 import Synthetic
-import SampleData
+
 
 export empty_model_params
 export dat_dir, sample_ce, perturb_params
