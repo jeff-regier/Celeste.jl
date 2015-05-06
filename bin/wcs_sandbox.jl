b3b3ffa0510032194378636b1823ee9fc914fa33
Ryan Giordano <rgiordan@gmail.com>
2015-05-06 16:27:12 -0700

    Poking around

--- bin/wcs_sandbox.jl
+++ bin/wcs_sandbox.jl
@@ -12,4 +12,12 @@ run_num = "003900"
 camcol_num = "6"
 frame_num = "0269"
 
-this_field = SDSS.load_field(field_dir, run_num, camcol_num, frame_num)
+this_field = SDSS.load_field(field_dir, run_num, camcol_num, frame_num);
+
+# Doesn't work:
+this_cat = SDSS.load_catalog(field_dir, run_num, camcol_num, frame_num);
+
+
+stamp_id = "164.4311-39.0359"
+blob = SDSS.load_stamp_blob(dat_dir, stamp_id);
+cat = SDSS.load_stamp_catalog(dat_dir, stamp_id, blob);
