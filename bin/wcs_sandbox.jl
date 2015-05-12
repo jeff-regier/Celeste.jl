cd1dcbd9ace5ec0f33596248e0db41f649674e09
Ryan Giordano <rgiordan@gmail.com>
2015-05-11 23:26:19 -0700

    Sky background interpolation

--- bin/wcs_sandbox.jl
+++ bin/wcs_sandbox.jl
@@ -10,6 +10,7 @@ using SampleData
 using FITSIO
 
 using DataFrames
+using Grid
 
 
 field_dir = joinpath(dat_dir, "sample_field")
@@ -34,7 +35,7 @@ read_header(photofield_fits[1])
 read_key(photofield_fits[1], "RUN")
 
 # The table.  You can only read one column at a time.
-read_fields = ["run", "rerun", "camcol", "skyversion", "field", "nStars"]
+read_fields = ["run", "rerun", "camcol", "skyversion", "field", "nStars", "gain", "darkVariance"]
 df = DataFrame()
 for field in read_fields
     df[DataFrames.identifier(field)] = read(photofield_fits[2], field)
@@ -52,11 +53,32 @@ b = 3
 b_letter = ['u', 'g', 'r', 'i', 'z'][b]
 img_filename = "$field_dir/frame-$b_letter-$run_num-$camcol_num-$frame_num.fits"
 img_fits = FITS(img_filename)
+length(img_fits) # Should be 4
 
-length(img_fits)
-sky_image = read(img_fits[3], "ALLSKY");
-#spy(sky_image[:,:,1])  # This looks a bit strange.
+# This is the sky-subtracted and calibrated image.  There are no fields in the first header.
+processed_image = read(img_fits[1]);
 
+# This is the sky bacgkround:
+sky_image_raw = read(img_fits[3], "ALLSKY");
+sky_x = collect(read(img_fits[3], "XINTERP"));
+sky_y = collect(read(img_fits[3], "YINTERP"));
+
+# Combining the example from
+# http://data.sdss3.org/datamodel/files/BOSS_PHOTOOBJ/frames/RERUN/RUN/CAMCOL/frame.html
+# ...with the documentation from the IDL language:
+# http://www.exelisvis.com/docs/INTERPOLATE.html
+# ...we can see that we are supposed to interpret a point (sky_x[i], sky_y[j])
+# with associated row and column (if, jf) = (floor(sky_x[i]), floor(sky_y[j]))
+# as lying in the square spanned by the points
+# (sky_image_raw[if, jf], sky_image_raw[if + 1, jf + 1]).
+
+sky_grid_vals = (1:1.:size(sky_image_raw)[1], 1:1.:size(sky_image_raw)[2]);
+sky_grid = CoordInterpGrid(sky_grid_vals, sky_image_raw[:,:,1], BCnearest, InterpLinear);
+sky_image = [ sky_grid[x, y] for x in sky_x, y in sky_y ];
+
+
+# This is the calibration vector:
+calib_image = read(img_fits[2])
 
 
 
