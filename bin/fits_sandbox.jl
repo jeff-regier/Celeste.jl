fc0b1208429df2eecb6fb17a2609c86ac0921f5f
Ryan Giordano <rgiordan@gmail.com>
2015-05-26 16:08:11 -0700

    notes about the catalog
    Former-commit-id: 7150ce93a4585f5cbcff71e635d0facd4f3e4f91

--- bin/fits_sandbox.jl
+++ bin/fits_sandbox.jl
@@ -87,12 +87,148 @@ PSF.convert_gmm_to_celeste(gmm)
 #     gal_scale::Float64
 # end
 
+# Is this standard?
+bandname = 'r'
+bandnum = find(band_letters .== bandname)[1]
+
 cat_filename = "$field_dir/photoObj-$run_num-$camcol_num-$frame_num.fits"
 cat_fits = FITSIO.FITS(cat_filename)
 
+cat_hdu = cat_fits[2]
+
 # The second block has the objects, e.g.:
-read(cat_fits[2], "PHI_DEV_DEG")
-read(cat_fits[2], "RESOLVE_STATUS") # some kind of bitmap?
-unique(read(cat_fits[2], "OBJC_TYPE"))
+read(cat_hdu, "PHI_DEV_DEG")
+read(cat_hdu, "RESOLVE_STATUS") # some kind of bitmap?
+unique(read(cat_hdu, "OBJC_TYPE"))
+
+# Eliminate "bright" objects.
+# photo_flags1_map is defined in astrometry.net/sdss/common.py
+# ...where we see that photo_flags1_map['BRIGHT'] = 2
+const bright_bitmask = 2
+is_bright = read(cat_hdu, "objc_flags") & bright_bitmask .> 0
+has_child = read(cat_hdu, "nchild") .> 0
+
+# What does cutToPrimary mean?
+
+# What is useObjcType?
+
+# What does it mean for this to be -9999?
+prob_psf = read(cat_hdu, "prob_psf")[bandnum, :][:];
+[ (x, sum(prob_psf .== x)) for x=unique(prob_psf) ]
+
+# TODO: don't do a float comparision?
+is_star = prob_psf .== 1
+is_gal = prob_psf .== 0
+is_bad_psf = (prob_psf .< 0.) | (prob_psf .> 1.)
+
+# Unlike the python, record galaxy estimates even when l_gal = 0.
+# Is this the right thing to do?
+fracdev = read(cat_hdu, "fracdev")[bandnum, :][:];
+has_dev = fracdev .> 0.
+has_exp = fracdev .< 1.
+is_comp = has_dev & has_exp
+is_bad_fracdev = (fracdev .< 0.) | (fracdev .> 1)
+
+# WTF?  I believe we skip uncertain objects of ambiguous type.
+any(is_comp & is_star)
+any((!is_star) & (!is_gal))
+
+# What units are the star fluxes supposed to be in?
+starflux = read(cat_hdu, "psfflux")
+
+# Which of these should we record?
+compflux = read(cat_hdu, "cmodelflux")
+devflux = read(cat_hdu, "devflux")
+expflux = read(cat_hdu, "expflux")
+
+# What does fixedComposites mean? (defaults to false)
+
+# This is strange.  I thought fracdev would be the probability of
+# being a dev type galaxy and compflux would reflect that, but 
+# it does not:
+#
+# julia> hcat(compflux[1,:]', devflux[1,:]', expflux[1,:]', fracdev)
+# 981x4 Array{Float32,2}:
+#   compflux      devflux       expflux          fracdev
+#   910.639       910.734       910.639          0.863811
+#   907.784       907.902       907.784          0.859879
+#   907.807       907.931       907.807          0.857916
+#     0.667391      0.667391      0.30009        0.040005
+#     0.524619      0.524619      0.469452       0.0     
+#    -0.0597614    -0.059767     -0.0597614      0.0     
+#    18.1697       18.1697        9.6434         0.0     
+#  1716.65       1716.66       1716.57           0.79117
+
+ra = read(cat_hdu, "ra")
+dec = read(cat_hdu, "dec")
+
+# This is the "position".  In our catalog entry, what is it exactly?
+# RaDecPos(ra, dec)
+
+
+# Should we really multiply by -1?
+phi_dev_deg = read(cat_hdu, "phi_dev_deg") * -1
+phi_exp_deg = read(cat_hdu, "phi_exp_deg") * -1
+
+# What is the meaning of this comment?
+# MAGIC -- minimum size of galaxy.
+theta_dev = max(read(cat_hdu, "theta_dev"), 1./30.)
+theta_exp = max(read(cat_hdu, "theta_exp"), 1./30.)
+
+ab_exp = read(cat_hdu, "ab_exp")
+re_exp = theta_exp
+phi_exp = phi_exp_deg
+
+ab_dev = read(cat_hdu, "ab_dev")
+re_dev = theta_dev
+phi_dev = phi_dev_deg
+
+
+# What do we do for ab, angle, and scale when the 
+# galaxy is a mix of types?
+CatalogEntry(
+	[ra[row], dec[row]], # pos
+	true, # is_star
+    starflux[:, row], # star_fluxes
+    gal_flux, # gal_fluxes
+    fracdev[row], # gal_frac_dev
+    # gal_ab
+)
+    pos::Vector{Float64}
+    is_star::Bool
+    star_fluxes::Vector{Float64}
+    gal_fluxes::Vector{Float64}
+    gal_frac_dev::Float64
+    gal_ab::Float64
+    gal_angle::Float64
+    gal_scale::Float64
+end
+
+rows = collect(1:length(read(cat_hdu, "objid")))
+
+# Skip "bright" rows and those with prob_psf not in [0, 1].
+rows = rows[not_bright & no_child & (!is_bad)]
+catalog = Array(CatalogEntry, length(rows));
+i = 1
+
+sum(is_bad & (!(not_bright & no_child)))
+for row=rows
+
+	gal_flux = zeros(length(band_letters))
+	if is_comp:
+		gal_flux = compflux[row, :]
+	elseif has_dev:
+		gal_flux = devflux[row, :]
+	elseif has_exp:
+		exp_flux = expflux[row, :]
+	else:
+		error("Should skip rows with a bad devflux.")
+	end
+
+
+end
+
+# Note that I don't think we need / use the brightness
+# values, so don't need flux2bright = nmgy2bright
 
 
