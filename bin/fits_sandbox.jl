9ff3a0c4854f5950d87cc96a25bb942c226f0488
Ryan Giordano <rgiordan@gmail.com>
2015-05-25 18:40:40 -0700

    comment
    Former-commit-id: 3f32edce6381e5395c5d277db3b0ba6092363c7b

--- bin/fits_sandbox.jl
+++ bin/fits_sandbox.jl
@@ -15,12 +15,10 @@ using SDSS
 import PyPlot
 
 
-
 # Some examples of the SDSS fits functions.
 
 # Blob for comparison
 blob, mp, one_body = gen_sample_galaxy_dataset();
-
 
 field_dir = joinpath(dat_dir, "sample_field")
 run_num = "003900"
@@ -49,21 +47,28 @@ sum(isnan(masked_nelec))
 # Load and fit a mixture of Gaussians to the psf
 
 rrows, rnrow, rncol, cmat = SDSS.load_psf_data(field_dir, run_num, camcol_num, frame_num, 1);
+
 psf = SDSS.get_psf_at_point(1., 1., rrows, rnrow, rncol, cmat);
 gmm = SDSS.fit_psf_gaussians(psf);
 
 gmm_fit = sum(psf) * Float64[ SDSS.evaluate_gmm(gmm, Float64[x, y]')[1] for x=1:size(psf, 1), y=1:size(psf, 2) ]
 
-PyPlot.matshow(gmm_fit)
-PyPlot.title("fit")
+if false
+	PyPlot.matshow(gmm_fit)
+	PyPlot.title("fit")
 
-PyPlot.matshow(psf)
-PyPlot.title("psf")
+	PyPlot.matshow(psf)
+	PyPlot.title("psf")
 
-PyPlot.matshow(psf - gmm_fit)
-PyPlot.title("diff")
+	PyPlot.matshow(psf - gmm_fit)
+	PyPlot.title("diff")
+end
 
 sum((psf - gmm_fit) ^ 2)
 
 SDSS.convert_gmm_to_celeste(gmm)
 
+
+# Load the catalog entry for a field.
+
+ 
