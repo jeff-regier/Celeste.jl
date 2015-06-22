9fc1f7b9e064be67f60466566c68b7bc47b75030
Ryan Giordano <rgiordan@gmail.com>
2015-06-22 13:47:44 -0700

    Making Image object
    Former-commit-id: ebf64d2f2eafaba3f9e45f8c834a4d40d9819977

--- bin/fits_sandbox.jl
+++ bin/fits_sandbox.jl
@@ -4,20 +4,16 @@ using CelesteTypes
 using DataFrames
 using SampleData
 
-using SDSS
+import SDSS
 import PSF
 import FITSIO
 import PyPlot
 
 # Some examples of the SDSS fits functions.
-
-# Blob for comparison
-blob, mp, one_body = gen_sample_galaxy_dataset();
-
 field_dir = joinpath(dat_dir, "sample_field")
 run_num = "003900"
 camcol_num = "6"
-frame_num = "0269"
+field_num = "0269"
 
 #############
 # Load the catalog
@@ -33,48 +29,38 @@ stamp_cat_entries_df =
 	SDSS.load_stamp_catalog_df(SampleData.dat_dir, "s82-5.0073-0.0739", blob);
 stamp_catalog = SDSS.load_stamp_catalog(SampleData.dat_dir, "s82-5.0073-0.0739", blob);
 
-cat_df = SDSS.load_catalog_df(field_dir, run_num, camcol_num, frame_num);
-# Not the right blob, just to test:
+cat_df = SDSS.load_catalog_df(field_dir, run_num, camcol_num, field_num);
+
+blob = SDSS.load_sdss_blob(field_dir, run_num, camcol_num, field_num);
 cat_entries = SDSS.convert_catalog_to_celeste(cat_df, blob)
 
+
 #######################
-# Load the image data
-band_gain, band_dark_variance = SDSS.load_photo_field(field_dir, run_num, camcol_num, frame_num)
+# WCS stuff.  See
+# https://github.com/JuliaAstro/FITSIO.jl/issues/39
 
-b = 1
-nelec, calib_col, sky_grid = SDSS.load_raw_field(field_dir, run_num, camcol_num, frame_num, b, band_gain[b]);
+b_letter = band_letters[b]
 
-# Get the masked image with and without the (probably buggy) python indexing.
-masked_nelec_bad = deepcopy(nelec);
-SDSS.mask_image!(masked_nelec_bad, field_dir, run_num, camcol_num, frame_num, b, python_indexing=true);
-sum(isnan(masked_nelec_bad))
+img_filename = "$field_dir/frame-$b_letter-$run_num-$camcol_num-$field_num.fits"
+img_fits = FITSIO.FITS(img_filename)
+@assert length(img_fits) == 4
 
-masked_nelec = deepcopy(nelec);
-SDSS.mask_image!(masked_nelec, field_dir, run_num, camcol_num, frame_num, b);
-sum(isnan(masked_nelec))
+fits_file = FITSIO.Libcfitsio.fits_open_file(img_filename)
+header_str = FITSIO.Libcfitsio.fits_hdr2str(fits_file)
+((wcs,),nrejected) = WCSLIB.wcspih(header_str)
 
+naxis = FITSIO.read_key(img_fits[1], "NAXIS")[1]
+@assert naxis == 2
 
-###############
-# Load and fit a mixture of Gaussians to the psf
+# cdelt = [FITSIO.read_key(img_fits[1], "NAXIS")[1]; FITSIO.read_key(img_fits[1], "NAXIS")[1]]
+ctype = [FITSIO.read_key(img_fits[1], "CTYPE1")[1], FITSIO.read_key(img_fits[1], "CTYPE2")[1]]
+crpix = [FITSIO.read_key(img_fits[1], "CRPIX1")[1], FITSIO.read_key(img_fits[1], "CRPIX2")[1]]
+crval = [FITSIO.read_key(img_fits[1], "CRVAL1")[1], FITSIO.read_key(img_fits[1], "CRVAL2")[1]]
 
-rrows, rnrow, rncol, cmat = SDSS.load_psf_data(field_dir, run_num, camcol_num, frame_num, 1);
-
-psf = PSF.get_psf_at_point(1., 1., rrows, rnrow, rncol, cmat);
-gmm = PSF.fit_psf_gaussians(psf);
-
-gmm_fit = sum(psf) * Float64[ PSF.evaluate_gmm(gmm, Float64[x, y]')[1] for x=1:size(psf, 1), y=1:size(psf, 2) ]
-
-if false
-	PyPlot.matshow(gmm_fit)
-	PyPlot.title("fit")
-
-	PyPlot.matshow(psf)
-	PyPlot.title("psf")
-
-	PyPlot.matshow(psf - gmm_fit)
-	PyPlot.title("diff")
-end
-
-sum((psf - gmm_fit) ^ 2)
-
-PSF.convert_gmm_to_celeste(gmm)
+# I don't know what the rest of these things should be, and the others are just a guess.
+w = wcsprm(naxis; 
+           cdelt = [-0.066667, 0.066667],
+           ctype = ctype,
+           crpix = crpix,
+           crval = val,
+           pv    = [pvcard(2, 1, 45.0)])
