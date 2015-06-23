cbb7bbd9b41add08de4b99c9f84e33ac90c9e7fd
Ryan Giordano <rgiordan@gmail.com>
2015-06-23 14:43:45 -0700

    Viewing in R

--- bin/view_images.R
+++ bin/view_images.R
@@ -3,8 +3,46 @@ library(dplyr)
 library(reshape2)
 library(gridExtra)
 
-d <- as.matrix(read.csv("/tmp/frame-u-003900-6-0269.csv", header=F))
+dcat <- read.csv("/tmp/catalog-003900-6-0269.csv", header=T)
 
-dcat <- read.csv("/tmp/catalog-u-003900-6-0269.csv", header=T)
+band <- 3
+band.letters <- c("u", "g", "r", "i", "z")
+file_name <- sprintf("/tmp/frame-%s-003900-6-0269.csv", band.letters[band])
+du <- as.matrix(read.csv(file_name, header=F))
+colnames(du) <- 1:ncol(du)
+rownames(du) <- 1:nrow(du)
+du_melt <- melt(du)
+names(du_melt) <- c("row", "col", "value")
 
-image(d[1:100, 1:100])
+band.letter <- band.letters[band]
+cat.px.x.col <- sprintf("pix_x_%d", band)
+cat.px.y.col <- sprintf("pix_y_%d", band)
+
+img_xlim <- c(150, 350)
+img_ylim <- c(300, 450)
+this_cat <- filter(dcat,
+                  dcat[[cat.px.x.col]]  >= img_xlim[1],
+                  dcat[[cat.px.x.col]]  <= img_xlim[2],
+                  dcat[[cat.px.y.col]]  >= img_ylim[1],
+                  dcat[[cat.px.y.col]]  <= img_ylim[2])
+this_cat$x <- this_cat[[cat.px.x.col]]
+this_cat$y <- this_cat[[cat.px.y.col]]
+
+this_du <- filter(du_melt,
+                  du_melt[["row"]]  >= img_xlim[1],
+                  du_melt[["row"]]  <= img_xlim[2],
+                  du_melt[["col"]]  >= img_ylim[1],
+                  du_melt[["col"]]  <= img_ylim[2])
+
+grid.arrange(
+  ggplot() +
+    geom_raster(data=this_du, aes(x=row, y=col, fill=value)) +
+    geom_point(data=this_cat, aes(x=x, y=y, color=is_star), size=5) +
+    scale_fill_continuous(low="#000000", high="#FFFFFF", na.value = "purple")
+  ,
+  ggplot() +
+    geom_raster(data=this_du, aes(x=row, y=col, fill=value)) +
+    scale_fill_continuous(low="#000000", high="#FFFFFF", na.value = "purple")
+  ,
+  ncol=2
+)
