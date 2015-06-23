d26faefc1f584250b3c3eb48c8294ad948cf5982
Ryan Giordano <rgiordan@gmail.com>
2015-06-23 14:43:45 -0700

    Viewing in R
    Former-commit-id: cbb7bbd9b41add08de4b99c9f84e33ac90c9e7fd

--- bin/fits_sandbox.jl
+++ bin/fits_sandbox.jl
@@ -32,55 +32,40 @@ b_letter = band_letters[b]
 blob = SDSS.load_sdss_blob(field_dir, run_num, camcol_num, field_num);
 cat_df = SDSS.load_catalog_df(field_dir, run_num, camcol_num, field_num);
 cat_entries = SDSS.convert_catalog_to_celeste(cat_df, blob);
+cat_coords = convert(Array{Float64}, cat_df[[:ra, :dec]])'
 
+# Write pixel coordinates to the catalog
+for b=1:5
+	pix_coords = WCSLIB.wcss2p(blob[b].wcs, cat_coords)
+	cat_df[symbol("pix_x_$(b)")] = pix_coords[1,:][:]
+	cat_df[symbol("pix_y_$(b)")] = pix_coords[2,:][:]
+end
+writetable("/tmp/catalog-$run_num-$camcol_num-$field_num.csv", cat_df)
 
 # Write to csv for easy viewing.
 for b=1:5
 	b_letter = band_letters[b]
 	writedlm("/tmp/frame-$b_letter-$run_num-$camcol_num-$field_num.csv", blob[b].pixels, ',')
 end
-writetable("/tmp/catalog-$b_letter-$run_num-$camcol_num-$field_num.csv", cat_df)
 
 
 #######################
 # WCS stuff.  See
 # https://github.com/JuliaAstro/FITSIO.jl/issues/39
 
-function get_wcs(img_filename)
-	img_fits = FITSIO.FITS(img_filename)
-	@assert length(img_fits) == 4
-	header_str = FITSIO.read_header(img_fits[1], ASCIIString)
-	((wcs,),nrejected) = WCSLIB.wcspih(header_str)
-	close(img_fits)
-	wcs
-end
-
 b = 1
 b_letter = band_letters[b]
-
-
 @assert 1 <= b <= 5
 b_letter = band_letters[b]
 
 # A bug in my FITSIO change:
 img_filename = "$field_dir/frame-$b_letter-$run_num-$camcol_num-$field_num.fits"
 img_fits = FITSIO.FITS(img_filename)
+ctype = [FITSIO.read_key(img_fits[1], "CTYPE1")[1], FITSIO.read_key(img_fits[1], "CTYPE2")[1]]
 foo = read_header(img_fits[1]);
 bar = read_header(img_fits[2]);
 header_str = FITSIO.read_header(img_fits[1], ASCIIString)
 close(img_fits)
-
-img_filename = "$field_dir/frame-$b_letter-$run_num-$camcol_num-$field_num.fits"
-img_fits = FITSIO.FITS(img_filename);
-sky_image_raw = read(img_fits[3], "ALLSKY");
-header_str = FITSIO.read_header(img_fits[1], ASCIIString);
-((wcs,),nrejected) = WCSLIB.wcspih(header_str);
-close(img_fits);
-
-wcs2 = get_wcs(img_filename);
-
-band_gain, band_dark_variance = SDSS.load_photo_field(field_dir, run_num, camcol_num, field_num);
-nelec, calib_col, sky_grid, sky_x, sky_y, wcs4, sdss_header_str = SDSS.load_raw_field(field_dir, run_num, camcol_num, field_num, b, band_gain[b]);
 
 world_coords = Array(Float64, 2, nrow(cat_df))
 for n=1:nrow(cat_df)
@@ -91,28 +76,13 @@ H = blob[b].H
 W = blob[b].W
 pixcrd = Float64[0 0; 0 H; 0 W; H W]'
 
-# Why is this not the same?
-hcat(WCSLIB.wcss2p(blob[b].wcs, world_coords)'[1:20,:],
-     WCSLIB.wcss2p(wcs, world_coords)'[1:20,:],
-     WCSLIB.wcss2p(wcs2, world_coords)'[1:20,:],
-     WCSLIB.wcss2p(wcs4, world_coords)'[1:20,:]
-     )
-
-hcat(WCSLIB.wcsp2s(blob[b].wcs, pixcrd)',
-     WCSLIB.wcsp2s(wcs, pixcrd)',
-     WCSLIB.wcsp2s(wcs2, pixcrd)')
-
+WCSLIB.wcss2p(blob[b].wcs, world_coords)'
 
 [ unsafe_load(blob[b].wcs.crval, i) for i=1:2 ]
-[ unsafe_load(wcs.crval, i) for i=1:2 ]
-
 [ unsafe_load(blob[b].wcs.cd, i) for i=1:4 ]
-[ unsafe_load(wcs3.cd, i) for i=1:4 ]
-[ unsafe_load(wcs.cd, i) for i=1:4 ]
 
 
-
-# Other way to read it in?
+# Other way to read it in?  This doesn't seem to work.
 naxis = FITSIO.read_key(img_fits[1], "NAXIS")[1]
 @assert naxis == 2
 
