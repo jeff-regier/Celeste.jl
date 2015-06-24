44166c8a06976025a8cb366a35f0c8f1f5f031e8
Ryan Giordano <rgiordan@gmail.com>
2015-06-24 11:38:35 -0700

    Cleaning up python psf script

--- bin/get_astrometry_psf.py
+++ bin/get_astrometry_psf.py
@@ -1,79 +1,35 @@
 # Based on util/test-psf.py in astrometry.net:
+import argparse
+
 from astrometry.util import sdss_psf
 from astrometry.sdss import *
 from numpy import *
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--ps_file', type=str,
+	                help=('The psField file to read (no hyphens, only underscores are '
+	                	  'allowed in the filename for some reason).'))
+parser.add_argument('--band', type=int, help='The band (a number from 0 to 4).')
+parser.add_argument('--row', type=float, help='The row at which to evaluate the psf (0-indexed).')
+parser.add_argument('--col', type=float, help='The column at which to evaluate the psf (0-indexed).')
+parser.add_argument('--destination_file', type=str, help='The name of the output csv file.')
+
+
+args = parser.parse_args()
+
+if not args.ps_file:
+	# Assume we're in interacive mode.
+	args.ps_file = '/tmp/psField_003900_6_0269.fit'
+	args.band = 0
+	args.col = 1024.0 - 1
+	args.row = 744.5 - 1
+	args.destination_file = '/tmp/py_psf.csv'
 
 # Note that pyfits insists on underscores, not hyphens.
-ps_filename = '/tmp/psField_003900_6_0269.fit'
-psfield = pyfits.open(ps_filename)
+psfield = pyfits.open(args.ps_file)
 
-x = 1024.0 - 1
-y = 744.5 - 1
-
-band = 0
-raw_psf = sdss_psf.sdss_psf_at_points(psfield[band + 1], x, y)
-raw_psf[0].max()
-
-###################
-# The raw code:
-hdu = psfield[band + 1]
-
-rtnscalar = True
-x = atleast_1d(x)
-y = atleast_1d(y)
-
-psf = table_fields(hdu.data)
-
-psfimgs = None
-(outh, outw) = (None,None)
-
-# From the IDL docs:
-# http://photo.astro.princeton.edu/photoop_doc.html#SDSS_PSF_RECON
-#   acoeff_k = SUM_i{ SUM_j{ (0.001*ROWC)^i * (0.001*COLC)^j * C_k_ij } }
-#   psfimage = SUM_k{ acoeff_k * RROWS_k }
-
-k = 0
-# for k in range(len(psf)):
-nrb = psf.nrow_b[k]
-ncb = psf.ncol_b[k]
-
-c = psf.c[k].reshape(5, 5)
-c = c[:nrb,:ncb]
-
-(gridi,gridj) = meshgrid(range(nrb), range(ncb))
-
-# rrows is the vectorized image.
-i = 0
-xi = x
-yi = y
-psfimgs = [ zeros_like(psf.rrows[k]) ]
-(outh,outw) = (psf.rnrow[k], psf.rncol[k])
-for k in range(len(psf)):
-	nrb = psf.nrow_b[k]
-	ncb = psf.ncol_b[k]
-	c = psf.c[k].reshape(5, 5)
-	c = c[:nrb,:ncb]
-	(gridi,gridj) = meshgrid(range(nrb), range(ncb))
-	acoeff_k = sum(((0.001 * xi)**gridi * (0.001 * yi)**gridj * c))
-	if True: # DEBUG
-		print '--------------------------\n'
-		print 'coeffs:', (0.001 * xi)**gridi * (0.001 * yi)**gridj
-		print 'c:', c
-		for (coi,ci) in zip(((0.001 * xi)**gridi * (0.001 * yi)**gridj).ravel(), c.ravel()):
-			print 'co %g, c %g' % (coi,ci)
-		print 'acoeff_k', acoeff_k
-	this_image = acoeff_k * psf.rrows[k]
-	psfimgs[i] += this_image
-	print sum(this_image)
-
-psfimgs = [img.reshape((outh,outw)) for img in psfimgs]
-savetxt("/tmp/py_psf.csv", psfimgs[0], delimiter=",")
-
-
-if rtnscalar:
-	return psfimgs[0]
-return psfimgs
-
-for k in range(4):
-	savetxt("/tmp/rrows_%d.csv" % k, psf.rrows[k], delimiter=",")
+print 'Reading psf:'
+raw_psf = sdss_psf.sdss_psf_at_points(psfield[args.band + 1], args.row, args.col)
+savetxt(args.destination_file, raw_psf, delimiter=",")
+print 'Done.  Output written to %s' % args.destination_file
