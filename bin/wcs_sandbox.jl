b11c3faf3799f91591ea903d1ace6805f757d07d
Ryan Giordano <rgiordan@gmail.com>
2015-07-03 14:54:24 -0700

    Re-centering world coordinates
    Former-commit-id: 525ceb39d447034e55d9fd2862ede1ad66142b89

--- bin/wcs_sandbox.jl
+++ bin/wcs_sandbox.jl
@@ -30,6 +30,13 @@ stamp_blob = SDSS.load_stamp_blob(dat_dir, "5.0073-0.0739");
 
 original_blob = SDSS.load_sdss_blob(field_dir, run_num, camcol_num, field_num);
 original_cat_df = SDSS.load_catalog_df(field_dir, run_num, camcol_num, field_num);
+original_crpix_band = Float64[unsafe_load(original_blob[b].wcs.crpix, i) for i=1:2, b=1:5];
+function reset_crpix!(blob)
+	for b=1:5
+		unsafe_store!(blob[b].wcs.crpix, original_crpix_band[1, b], 1)
+		unsafe_store!(blob[b].wcs.crpix, original_crpix_band[2, b], 2)
+	end
+end
 
 cat_cols = [ :objid, :ra, :dec, :is_star, :frac_dev, :compflux_r, :psfflux_r ]
 original_cat_df[:, cat_cols]
@@ -38,22 +45,17 @@ original_cat_df[:, cat_cols]
 # Select an object.
 
 cat_loc = convert(Array{Float64}, original_cat_df[[:ra, :dec]]);
-cat_pix = Util.world_to_pixel(original_blob[4].wcs, cat_loc)
-
 obj_df = original_cat_df[[:objid, :is_star, :is_gal, :psfflux_r, :compflux_r]]
 sort(obj_df[obj_df[:is_gal] .== true, :], cols=:compflux_r, rev=true)
 sort(obj_df[obj_df[:is_gal] .== false, :], cols=:psfflux_r, rev=true)
-
 
 #objid = "1237662226208063597" # A star that was obviously miscentered
 #objid = "1237662226208063499" # A bright star also with lots of bad pixels
 #objid = "1237662226208063632" # A bright galaxy
 #objid = "1237662226208063541" # A bright star but with lots of bad pixels
 #objid = "1237662226208063551" # A bright star but with lots of bad pixels
-objid = "1237662226208063565"
+objid = "1237662226208063565" # A bright star
 
-obj_row = original_cat_df[:objid] .== objid;
-obj_loc = convert(Array, original_cat_df[obj_row, [:ra, :dec]])'[:]
 
 #sub_rows_x = 1:150
 #sub_rows_y = 1:150
@@ -61,8 +63,12 @@ obj_loc = convert(Array, original_cat_df[obj_row, [:ra, :dec]])'[:]
 width = 8
 
 blob = deepcopy(original_blob);
+reset_crpix!(blob);
 cat_df = deepcopy(original_cat_df);
 cat_loc = convert(Array{Float64}, cat_df[[:ra, :dec]]);
+obj_row = original_cat_df[:objid] .== objid;
+obj_loc = convert(Array, original_cat_df[obj_row, [:ra, :dec]])'[:]
+#[ Util.world_to_pixel(blob[b].wcs, obj_loc) for b=1:5]
 entry_in_range = Bool[true for i=1:size(cat_loc, 1) ];
 x_ranges = zeros(2, 5)
 y_ranges = zeros(2, 5)
@@ -77,19 +83,29 @@ for b=1:5
 	x_ranges[:, b] = Float64[x_min, x_max]
 	y_ranges[:, b] = Float64[y_min, y_max]
 
-	blob[b].pixels = blob[b].pixels[sub_rows_x, sub_rows_y]
-	blob[b].H = size(blob[b].pixels, 1)
-	blob[b].W = size(blob[b].pixels, 2)
 	wcs_range = Util.world_to_pixel(blob[b].wcs, cat_loc)
 	entry_in_range = entry_in_range &
 		(x_min .<= wcs_range[:, 1] .<= x_max) &
 		(y_min .<= wcs_range[:, 2] .<= y_max)
+
+	# Re-center the WCS coordinates
+	crpix = original_crpix_band[:, b]
+	unsafe_store!(blob[b].wcs.crpix, crpix[1] - x_min + 1, 1)
+	unsafe_store!(blob[b].wcs.crpix, crpix[2] - y_min + 1, 2)
+	
+	blob[b].pixels = blob[b].pixels[sub_rows_x, sub_rows_y]
+	blob[b].H = size(blob[b].pixels, 1)
+	blob[b].W = size(blob[b].pixels, 2)
 end
 cat_df = cat_df[entry_in_range, :]
 cat_entries = SDSS.convert_catalog_to_celeste(cat_df, blob);
 cat_loc = convert(Array{Float64}, cat_df[[:ra, :dec]]);
 initial_mp = ModelInit.cat_init(cat_entries, patch_radius=20.0, tile_width=5);
 
+# Check the re-centering
+for b=1:5
+	println(Util.world_to_pixel(blob[b].wcs, obj_loc))
+end
 
 ##############################
 # Fit the image.
@@ -109,13 +125,31 @@ end
 
 function get_brightness(mp::ModelParams)
 	brightness = [ElboDeriv.SourceBrightness(mp.vp[s]) for s in 1:mp.S];
-	brightness_vals = [ Float64[b.E_l_a[i, j].v for i=1:size(b.E_l_a, 1), j=1:size(b.E_l_a, 2)] for b in brightness]
+	brightness_vals = [ Float64[b.E_l_a[i, j].v for
+		i=1:size(b.E_l_a, 1), j=1:size(b.E_l_a, 2)] for b in brightness]
 	brightness_vals
 end
 
 function display_cat(cat_entry::CatalogEntry)
 	[ println("$name: $(cat_entry.(name))") for name in names(cat_entry) ]
 end
+
+ids_free_names = Array(ASCIIString, length(ids_free))
+for (name in names(ids_free)) 
+	inds = ids_free.(name)
+	for i = 1:length(inds)
+		ids_free_names[inds[i]] = "$(name)_$(i)"
+	end
+end
+
+ids_names = Array(ASCIIString, length(ids))
+for (name in names(ids)) 
+	inds = ids.(name)
+	for i = 1:length(inds)
+		ids_names[inds[i]] = "$(name)_$(i)"
+	end
+end
+
 
 #include("src/ElboDeriv.jl"); include("src/OptimizeElbo.jl")
 mp = deepcopy(initial_mp);
@@ -125,13 +159,82 @@ compare_solutions(mp, initial_mp)
 display_cat(cat_entries[1]);
 get_brightness(mp)
 
+
+
 # It is not computing these derivatives?!?
 mp = deepcopy(initial_mp);
-omitted_ids = setdiff(1:length(UnconstrainedParams), [ids_free.u, ids_free.e_dev])
-mp.vp[1][ids.u] = [0., 0.] 
-mp.vp[1][ids.e_dev] = 0.02
+#mp.vp[1][ids.u] = [0., 0.] 
+#mp.vp[1][ids.e_dev] = 0.02
+#omitted_ids = setdiff(1:length(UnconstrainedParams), [ids_free.u, ids_free.e_dev])
 #omitted_ids = setdiff(1:length(UnconstrainedParams), [ids_free.e_dev])
-OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp, Transform.rect_transform, omitted_ids=omitted_ids)
+#OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp, Transform.rect_transform, omitted_ids=omitted_ids)
+elbo = ElboDeriv.elbo_likelihood(blob, mp);
+DataFrame(name=ids_names, elbo=elbo.d[:])
+
+###################
+
+b = 4
+ret = zero_sensitive_float(CanonicalParams, mp.S)
+img = blob[b]
+ElboDeriv.elbo_likelihood!(img, mp, ret)
+
+accum = ret
+accum.v += -sum(lfact(img.pixels[!isnan(img.pixels)]))
+
+star_mcs, gal_mcs = ElboDeriv.load_bvn_mixtures(img.psf, mp, img.wcs)
+
+sbs = [ElboDeriv.SourceBrightness(mp.vp[s]) for s in 1:mp.S]
+
+WW = int(ceil(img.W / mp.tile_width))
+HH = int(ceil(img.H / mp.tile_width))
+e_image = zeros(img.H, img.W)
+for ww in 1:WW, hh in 1:HH
+	tile = ImageTile(hh, ww, img)
+	# might get a speedup from subsetting the mp here
+
+	tile_sources = ElboDeriv.local_sources(tile, mp)
+	h_range, w_range = ElboDeriv.tile_range(tile, mp.tile_width)
+	println("$tile_sources $h_range $w_range")
+
+	# fs0m and fs1m accumulate contributions from all sources
+	fs0m = zero_sensitive_float(StarPosParams)
+	fs1m = zero_sensitive_float(GalaxyPosParams)
+
+	tile_S = length(tile_sources)
+	E_G = zero_sensitive_float(CanonicalParams, tile_S)
+	var_G = zero_sensitive_float(CanonicalParams, tile_S)
+
+	# Iterate over pixels that are not NaN.
+	for w in w_range, h in h_range
+	    this_pixel = tile.img.pixels[h, w]
+	    if !isnan(this_pixel)
+	        clear!(E_G)
+	        E_G.v = tile.img.epsilon
+	        clear!(var_G)
+
+	        # TODO: could you go back to pixel coordinates here?
+	        # Convert the pixel location to world coordinates.
+	        #m_pos = Util.pixel_to_world(tile.img.wcs, Float64[h, w])
+	        m_pos = Float64[h, w]
+	        wcs_jacobian = Util.pixel_world_jacobian(tile.img.wcs, m_pos)
+	        for child_s in 1:length(tile_sources)
+	            parent_s = tile_sources[child_s]
+	            ElboDeriv.accum_pixel_source_stats!(sbs[parent_s], star_mcs, gal_mcs,
+	                mp.vp[parent_s], child_s, parent_s, m_pos, tile.img.b,
+	                fs0m, fs1m, E_G, var_G, wcs_jacobian)
+	        end
+	        println(E_G.v)
+	        e_image[h, w] = E_G.v
+	        ElboDeriv.accum_pixel_ret!(tile_sources, this_pixel, tile.img.iota,
+	            E_G, var_G, accum)
+	    end
+	end
+end
+
+
+[ round(e_image[i, j] - blob[b].pixels[i, j] / blob[b].iota, 1) for i=1:img.H, j=1:img.W ]
+
+
 
 
 #############################
