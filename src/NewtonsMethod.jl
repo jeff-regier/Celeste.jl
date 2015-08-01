de95be4258ecb0f29c58cdb288545c15cc1825c5
Ryan Giordano <rgiordan@gmail.com>
2015-07-31 18:37:13 -0700

    Wrapper for NM

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,32 +1,46 @@
 
 using OptimizeElbo
-using Transform
+import Transform
+import Optim
 
 # Put this in OptimizeElbo once it's working.
-function maximize_f_newton(f::Function, blob::Blob, mp::ModelParams, transform::DataTransform,
-                           lbs::Array{Float64, 1}, ubs::Array{Float64, 1};
-                           omitted_ids=Int64[], xtol_rel = 1e-7, ftol_abs = 1e-6)
+function maximize_f_newton(f::Function, mp::ModelParams, transform::Transform.DataTransform;
+                           omitted_ids=Int64[], xtol_rel = 1e-7, ftol_abs = 1e-6, verbose=false, hess_reg=2.0, max_iters=100)
 
     kept_ids = setdiff(1:length(UnconstrainedParams), omitted_ids)
     x0 = transform.vp_to_vector(mp.vp, omitted_ids)
 
-    obj_wrapper = ObjectiveWrapperFunctions(mp -> f(blob, mp), mp, transform, kept_ids, omitted_ids);
+    optim_obj_wrap = OptimizeElbo.ObjectiveWrapperFunctions(f, mp, transform, kept_ids, omitted_ids);
+    optim_obj_wrap.state.scale = -1.0 # For minimization, which is required by the linesearch algorithm.
+    optim_obj_wrap.state.verbose = verbose
 
-    opt = Opt(:LD_LBFGS, length(x0))
-    for i in 1:length(x0)
-        if !(lbs[i] <= x0[i] <= ubs[i])
-            println("coordinate $i falsity: $(lbs[i]) <= $(x0[i]) <= $(ubs[i])")
+    function f_hess_reg!(x, new_hess)
+        hess = optim_obj_wrap.f_ad_hessian(x)
+        hess_ev = eig(hess)[1]
+        min_ev = minimum(hess_ev)
+        max_ev = maximum(hess_ev)
+
+        # Make it positive definite.
+        if min_ev < 0
+            verbose && println("Hessian is negative definite (min eigenvalue $(min_ev)).  Regularizing with $(hess_reg).")
+            hess += eye(length(x)) * abs(min_ev) * hess_reg
         end
-    end
-    lower_bounds!(opt, lbs)
-    upper_bounds!(opt, ubs)
-    max_objective!(opt, obj_wrapper.f_value_grad!)
-    xtol_rel!(opt, xtol_rel)
-    ftol_abs!(opt, ftol_abs)
-    (max_f, max_x, ret) = optimize(opt, x0)
 
-    iter_count = obj_wrapper.state.f_evals
-    println("got $max_f at $max_x after $iter_count iterations (returned $ret)\n")
+        new_hess[:,:] = hess
+    end
+
+    x0 = transform.vp_to_vector(mp_original.vp, omitted_ids);
+
+    # TODO: are xtol_rel and ftol_abs still good names?
+    nm_result = Optim.optimize(optim_obj_wrap.f_value, optim_obj_wrap.f_grad!, f_hess_reg!,
+                               x0, method=:newton, iterations=max_iters, xtol=xtol_rel, ftol=ftol_abs)
+
+    iter_count = optim_obj_wrap.state.f_evals
+    transform.vector_to_vp!(nm_result.minimum, mp.vp, omitted_ids);
+    max_f = -1.0 * nm_result.f_minimum
+    max_x = nm_result.minimum
+
+    println("got $max_f at $max_x after $iter_count function evaluations ($(nm_result.iterations) Newton steps)\n")
     iter_count, max_f, max_x, ret
 end
 
