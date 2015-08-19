4c82dffd56c73cd65225308b7a01b80d8d4b7d07
Ryan Giordano <rgiordan@gmail.com>
2015-08-19 13:37:00 -0700

    Fixing tests

--- test/test_optimization.jl
+++ test/test_optimization.jl
@@ -7,8 +7,7 @@ using Transform
 import OptimizeElbo
 
 function test_objective_wrapper()
-    # Note that due to the WCS transformation, location coordinates can't be done with autodiff.
-    omitted_ids = [ids_free.u];
+    omitted_ids = [];
     kept_ids = setdiff(1:length(ids_free), omitted_ids);
     blob, mp, body = SampleData.gen_two_body_dataset();
     trans = get_mp_transform(mp, loc_width=1.0);
@@ -88,23 +87,26 @@ end
 #########################################################
 
 
-function test_star_optimization(trans::DataTransform)
+function test_star_optimization()
     # TODO: this is failing due to bad scaling.
     blob, mp, body = gen_sample_star_dataset()
+    trans = get_mp_transform(mp, loc_width=1.0);
     OptimizeElbo.maximize_likelihood(blob, mp, trans)
     verify_sample_star(mp.vp[1], [10.1, 12.2])
 end
 
 
-function test_galaxy_optimization(trans::DataTransform)
+function test_galaxy_optimization()
     blob, mp, body = gen_sample_galaxy_dataset()
+    trans = get_mp_transform(mp, loc_width=1.0);
     OptimizeElbo.maximize_likelihood(blob, mp, trans, xtol_rel=0.0)
     verify_sample_galaxy(mp.vp[1], [8.5, 9.6])
 end
 
 
-function test_kappa_finding(trans::DataTransform)
+function test_kappa_finding()
     blob, mp, body = gen_sample_galaxy_dataset()
+    trans = get_mp_transform(mp, loc_width=1.0);
     omitted_ids = setdiff(1:length(UnconstrainedParams), ids_free.k[:])
 
     get_kl_gal_c() = begin
@@ -175,19 +177,19 @@ function test_bad_a_init()
     blob = Synthetic.gen_blob(blob0, [ce,])
 
     mp = ModelInit.cat_init([ce,])
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     mp.vp[1][ids.a] = [ 0.5, 0.5 ]
 
     omitted_ids = [ids_free.a]
-    OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+    OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp, trans, omitted_ids=omitted_ids)
 
     mp.vp[1][ids.a] = [ 0.8, 0.2 ]
     elbo_bad = ElboDeriv.elbo_likelihood(blob, mp)
     @test elbo_bad.d[ids.a[2], 1] > 0
 
     omitted_ids = setdiff(1:length(UnconstrainedParams), ids_free.a)
-    OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+    OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp, trans, omitted_ids=omitted_ids)
     @test mp.vp[1][ids.a[2]] >= 0.5
 
     mp2 = deepcopy(mp)
@@ -212,15 +214,17 @@ function test_likelihood_invariance_to_a()
     blob = Synthetic.gen_blob(blob0, [ce,])
 
     mp = ModelInit.cat_init([ce,])
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     mp.vp[1][ids.a] = [ 0.8, 0.2 ]
     omitted_ids = [ids_free.a, ids_free.r2[:]]
     OptimizeElbo.maximize_f(ElboDeriv.elbo_likelihood, blob, mp,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+        trans, omitted_ids=omitted_ids)
 
     mp2 = ModelInit.cat_init([ce,])
     mp2.vp[1][ids.a] = [ 0.2, 0.8 ]
     OptimizeElbo.maximize_f(ElboDeriv.elbo_likelihood, blob, mp2,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+        trans, omitted_ids=omitted_ids)
 
     mp.vp[1][ids.a] = [ 0.5, 0.5 ]
     mp2.vp[1][ids.a] = [ 0.5, 0.5 ]
@@ -250,15 +254,16 @@ function test_kl_invariance_to_a()
     end
 
     mp = ModelInit.cat_init([ce,])
+    trans = get_mp_transform(mp, loc_width=1.0);
     mp.vp[1][ids.a] = [ 0.2, 0.8 ]
     omitted_ids = [ids_free.a;]
     OptimizeElbo.maximize_f(kl_wrapper, blob, mp,
-        pixel_rect_transform, omitted_ids=omitted_ids, ftol_abs=1e-9)
+        trans, omitted_ids=omitted_ids, ftol_abs=1e-9)
 
     mp2 = ModelInit.cat_init([ce,])
     mp2.vp[1][ids.a] = [ 0.8, 0.2 ]
     OptimizeElbo.maximize_f(kl_wrapper, blob, mp2,
-        pixel_rect_transform, omitted_ids=omitted_ids, ftol_abs=1e-9)
+        trans, omitted_ids=omitted_ids, ftol_abs=1e-9)
 
     mp.vp[1][ids.a] = [ 0.5, 0.5 ]
     mp2.vp[1][ids.a] = [ 0.5, 0.5 ]
@@ -281,15 +286,17 @@ function test_elbo_invariance_to_a()
     blob = Synthetic.gen_blob(blob0, [ce,])
 
     mp = ModelInit.cat_init([ce,])
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     mp.vp[1][ids.a] = [ 0.8, 0.2 ]
     omitted_ids = [ids_free.a, ids_free.r2[:], ids_free.c2[:], ids_free.e_dev]
     OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+        trans, omitted_ids=omitted_ids)
 
     mp2 = ModelInit.cat_init([ce,])
     mp2.vp[1][ids.a] = [ 0.2, 0.8 ]
     OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp2,
-        pixel_rect_transform, omitted_ids=omitted_ids)
+        trans, omitted_ids=omitted_ids)
 
     mp.vp[1][ids.a] = [ 0.5, 0.5 ]
     mp2.vp[1][ids.a] = [ 0.5, 0.5 ]
@@ -301,15 +308,17 @@ function test_elbo_invariance_to_a()
 end
 
 
-function test_peak_init_galaxy_optimization(trans::DataTransform)
+function test_peak_init_galaxy_optimization()
     blob, mp, body = gen_sample_galaxy_dataset()
     mp = ModelInit.peak_init(blob)
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     OptimizeElbo.maximize_likelihood(blob, mp, trans)
     verify_sample_galaxy(mp.vp[1], [8.5, 9.6])
 end
 
 
-function test_peak_init_2body_optimization(trans::DataTransform)
+function test_peak_init_2body_optimization()
     srand(1)
     blob0 = SDSS.load_stamp_blob(dat_dir, "164.4311-39.0359")
 
@@ -320,6 +329,8 @@ function test_peak_init_2body_optimization(trans::DataTransform)
 
     blob = Synthetic.gen_blob(blob0, two_bodies)
     mp = ModelInit.peak_init(blob) #one giant tile, giant patches
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     @test mp.S == 2
 
     OptimizeElbo.maximize_likelihood(blob, mp, trans)
@@ -329,14 +340,15 @@ function test_peak_init_2body_optimization(trans::DataTransform)
 end
 
 
-function test_full_elbo_optimization(trans::DataTransform)
+function test_full_elbo_optimization()
     blob, mp, body = gen_sample_galaxy_dataset(perturb=true)
+    trans = get_mp_transform(mp, loc_width=1.0);
     OptimizeElbo.maximize_elbo(blob, mp, trans, xtol_rel=0.0)
     verify_sample_galaxy(mp.vp[1], [8.5, 9.6])
 end
 
 
-function test_real_stamp_optimization(trans::DataTransform)
+function test_real_stamp_optimization()
     blob = SDSS.load_stamp_blob(dat_dir, "5.0073-0.0739")
     cat_entries = SDSS.load_stamp_catalog(dat_dir, "s82-5.0073-0.0739", blob)
     bright(ce) = sum(ce.star_fluxes) > 3 || sum(ce.gal_fluxes) > 3
@@ -346,11 +358,12 @@ function test_real_stamp_optimization(trans::DataTransform)
     cat_entries = filter(inbounds, cat_entries)
 
     mp = ModelInit.cat_init(cat_entries)
+    trans = get_mp_transform(mp, loc_width=1.0);
     OptimizeElbo.maximize_elbo(blob, mp, trans, xtol_rel=0.0)
 end
 
 
-function test_bad_galaxy_init(trans::DataTransform)
+function test_bad_galaxy_init()
     stamp_id = "5.0624-0.1528"
     blob0 = SDSS.load_stamp_blob(ENV["STAMP"], stamp_id)
 
@@ -367,13 +380,14 @@ function test_bad_galaxy_init(trans::DataTransform)
     cat_primary = filter(only_center, cat_primary)
     @test length(cat_primary) == 1
 
+    mp_good_init = ModelInit.cat_init(cat_coadd)
+    trans = get_mp_transform(mp_good_init, loc_width=1.0);
+    OptimizeElbo.maximize_elbo(blob, mp_good_init, trans)
+    @test mp_good_init.vp[1][ids.a[2]] > .5
+
     mp_bad_init = ModelInit.cat_init(cat_primary)
     OptimizeElbo.maximize_f(ElboDeriv.elbo, blob, mp_bad_init, trans)
     @test mp_bad_init.vp[1][ids.a[2]] > .5
-
-    mp_good_init = ModelInit.cat_init(cat_coadd)
-    OptimizeElbo.maximize_elbo(blob, mp_good_init, trans)
-    @test mp_good_init.vp[1][ids.a[2]] > .5
 
     @test_approx_eq_eps mp_good_init.vp[1][ids.e_scale] mp_bad_init.vp[1][ids.e_scale] 0.2
     @test_approx_eq_eps mp_good_init.vp[1][ids.e_axis] mp_bad_init.vp[1][ids.e_axis] 0.2
@@ -384,6 +398,8 @@ end
 
 function test_color(trans::DataTransform)
     blob, mp, body = gen_sample_galaxy_dataset(perturb=true)
+    trans = get_mp_transform(mp, loc_width=1.0);
+
     # these are a bright star's colors
     mp.vp[1][ids.c1[:, 1]] = [2.42824, 1.13996, 0.475603, 0.283062]
     mp.vp[1][ids.c1[:, 2]] = [2.42824, 1.13996, 0.475603, 0.283062]
@@ -422,8 +438,12 @@ function test_quadratic_optimization(trans::DataTransform)
         val
     end
 
+    ###############
+    # TODO: this needs to be changed for the new transforms.
+
     # 0.5 is an innocuous value for all parameters.
     mp = empty_model_params(1)
+    trans = DataTransform
     n = length(CanonicalParams)
     mp.vp = convert(VariationalParams{Float64}, [fill(0.5, n) for s in 1:1])
     unused_blob = gen_sample_star_dataset()[1]
