585d42f07165bcf5072632ba5d59b629537b537e
Ryan Giordano <rgiordan@gmail.com>
2015-09-01 12:49:02 -0700

    Another patch bug

--- test/test_optimization.jl
+++ test/test_optimization.jl
@@ -28,7 +28,7 @@ end
 # trans = get_mp_transform(mp, loc_width=1.0);
 # OptimizeElbo.maximize_likelihood(tiled_blob, mp, trans, verbose=false)
 # verify_sample_star(mp.vp[1], [10.1, 12.2])
-# 
+#
 
 function verify_sample_galaxy(vs, pos)
     @test vs[ids.a[2]] >= 0.99
