cd0e1edb44555f70fbeb93e9dfdaf386f2bd13ad
Ryan Giordano <rgiordan@gmail.com>
2015-10-26 17:01:54 -0700

    Visualize the selected tiles

--- bin/process_image.jl
+++ bin/process_image.jl
@@ -3,11 +3,20 @@ using Celeste
 using PyPlot
 
 include(joinpath(Pkg.dir("Celeste"), "src/CelesteCluster.jl"))
-frame_jld_file = "initialzed_celeste_003900-6-0269.JLD"
+frame_jld_file = "initialzed_celeste_003900_6_0269.JLD"
 S = 20
-synthetic = true
+synthetic = false
 
-load_cluster_data();
+if synthetic
+  srand(1)
+  blob, original_mp, body, tiled_blob =
+    SampleData.gen_n_body_dataset(S, tile_width=10);
+else
+  img_dict = JLD.load(joinpath(dat_dir, frame_jld_file));
+  tiled_blob = img_dict["tiled_blob"];
+  original_mp = img_dict["mp_all"];
+end;
+mp = deepcopy(original_mp);
 
 
 # Trim the tile_sources for source s.
@@ -38,6 +47,29 @@ function trim_source_tiles!(s::Int64, mp::ModelParams{Float64})
 end
 
 
+function stitch_object_tiles(
+    s::Int64, b::Int64, mp::ModelParams{Float64}, tiled_blob::TiledBlob)
+
+  has_s = Bool[ s in mp.tile_sources[b][h, w] for h=1:H, w=1:W ];
+  tiles_s = tiled_blob[b][has_s];
+  h_range = Int[typemax(Int), typemin(Int)]
+  w_range = Int[typemax(Int), typemin(Int)]
+  for tile in tiles_s
+    h_range[1] = min(minimum(tile.h_range), h_range[1])
+    h_range[2] = max(maximum(tile.h_range), h_range[2])
+    w_range[1] = min(minimum(tile.w_range), w_range[1])
+    w_range[2] = max(maximum(tile.w_range), w_range[2])
+  end
+
+  image_s = fill(NaN, diff(h_range)[1] + 1, diff(w_range)[1] + 1);
+  for tile in tiles_s
+    image_s[tile.h_range - h_range[1] + 1, tile.w_range - w_range[1] + 1] =
+      tile.pixels
+  end
+  image_s
+end
+
+
 mp_1 = deepcopy(mp);
 mp_1.active_sources = [1];
 trim_source_tiles!(1, mp_1);
@@ -45,5 +77,18 @@ trim_source_tiles!(1, mp_1);
 [ sum([ 1 in tile for tile in mp.tile_sources[b]]) for b in 1:5]
 [ sum([ 1 in tile for tile in mp_1.tile_sources[b]]) for b in 1:5]
 
+for b=1:5
+  image_s = stitch_object_tiles(1, b, mp_1, tiled_blob);
+  matshow(image_s); colorbar(); title(b)
+end
+
+
+@time ElboDeriv.elbo(tiled_blob, mp_1);
 transform = Transform.get_mp_transform(mp_1);
 OptimizeElbo.maximize_elbo(tiled_blob, mp_1; verbose=true);
+
+
+
+
+
+# ok
