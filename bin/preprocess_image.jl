faf4861836cbe5bca1160f34e849c8d684fd3783
Ryan Giordano <rgiordan@gmail.com>
2015-11-09 22:47:25 -0800

    Preprocess a stripe82 image
    Former-commit-id: 3d725c794748562614bde93395640eed59369900

--- bin/preprocess_image.jl
+++ bin/preprocess_image.jl
@@ -5,11 +5,17 @@ using CelesteTypes
 using SampleData
 import JLD
 
-field_dir = joinpath(dat_dir, "sample_field")
-run_num = "003900"
-camcol_num = "6"
-field_num = "0269"
-tile_width = 10
+# field_dir = joinpath(dat_dir, "sample_field")
+# run_num = "003900"
+# camcol_num = "6"
+# field_num = "0269"
+
+field_dir = joinpath(dat_dir, "stripe82_fields")
+run_num = "000211"
+camcol_num = "4"
+field_num = "0227"
+
+tile_width = 20
 
 blob =
   SkyImages.load_sdss_blob(field_dir, run_num, camcol_num, field_num,
@@ -21,7 +27,7 @@ cat_entries = SkyImages.convert_catalog_to_celeste(cat_df, blob);
 # TODO: change fit_psf to true when you're sure it works.
 tiled_blob, mp_all =
   ModelInit.initialize_celeste(blob, cat_entries,
-    tile_width=5, fit_psf=true);
+    tile_width=tile_width, fit_psf=true);
 
 # The blob cannot be saved because it has a pointer to a C++ object.
 JLD.save(string("$dat_dir/initialzed_celeste_",
