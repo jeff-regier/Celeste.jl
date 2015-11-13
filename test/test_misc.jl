7358f24522f3fa0303234f111d24c8eae301b35d
Ryan Giordano <rgiordan@gmail.com>
2015-11-13 15:54:19 -0800

    Trim source tiles test

--- test/test_misc.jl
+++ test/test_misc.jl
@@ -236,7 +236,24 @@ end
 
 function test_trim_source_tiles()
   blob, mp, bodies, tiled_blob = gen_n_body_dataset(3);
-  
+
+  s = 1
+  trimmed_tiled_blob = ModelInit.trim_source_tiles(s, mp, tiled_blob);
+
+  loc_ids = ids.u
+  non_loc_ids = setdiff(1:length(ids), ids.u)
+  for b=1:5
+    s_tiles = SkyImages.find_source_tiles(s, b, mp)
+    mp.active_sources = [s];
+    elbo_full = ElboDeriv.elbo(tiled_blob, mp);
+    elbo_trim = ElboDeriv.elbo(trimmed_tiled_blob, mp);
+    @test_approx_eq_eps(
+      elbo_full.d[loc_ids, s] ./ elbo_trim.d[loc_ids, s],
+      fill(1.0, length(loc_ids)), 3e-2)
+    @test_approx_eq_eps(
+      elbo_full.d[non_loc_ids, s] ./ elbo_trim.d[non_loc_ids, s],
+      fill(1.0, length(non_loc_ids)), 2e-3)
+  end
 end
 
 function test_sky_noise_estimates()
