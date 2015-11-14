87abf9c3bd70b97d3190606be3a974b4270a8d35
Ryan Giordano <rgiordan@gmail.com>
2015-11-13 17:33:18 -0800

    Start working on brightness prior
    Former-commit-id: 3139e3fc439f3f0d631f1d8242ffbe1291846f97

--- src/KL.jl
+++ src/KL.jl
@@ -84,37 +84,37 @@ function gen_categorical_kl{NumType <: Number}(p2::Vector{NumType})
 end
 
 
-@doc """
-KL divergence between a pair of gamma distributions
-
-Args:
-- k2: the shape parameter for the second gamma distribution
-- theta2: the scale parameter for the second gamma distribution
-
-Returns:
-- a function the takes shape and scale parameters for the first
-  gamma distribution, and returns the KL divergence and its derivatives
-""" ->
-function gen_gamma_kl{NumType <: Number}(k2::NumType, theta2::NumType)
-    function this_gamma_lk{NumType2 <: Number}(k1::NumType2, theta1::NumType2)
-        digamma_k1 = digamma(k1)
-        theta_ratio = (theta1 - theta2) / theta2
-        shape_diff = k1 - k2
-
-        v = shape_diff * digamma_k1
-        v += -lgamma(k1) + lgamma(k2)
-        v += k2 * (log(theta2) - log(theta1))
-        v += k1 * theta_ratio
-
-        d_k1 = shape_diff * trigamma(k1)
-        d_k1 += theta_ratio
-
-        d_theta1 = -k2 / theta1
-        d_theta1 += k1 / theta2
-
-        v, (d_k1, d_theta1)
-    end
-end
+# @doc """
+# KL divergence between a pair of gamma distributions
+#
+# Args:
+# - k2: the shape parameter for the second gamma distribution
+# - theta2: the scale parameter for the second gamma distribution
+#
+# Returns:
+# - a function the takes shape and scale parameters for the first
+#   gamma distribution, and returns the KL divergence and its derivatives
+# """ ->
+# function gen_gamma_kl{NumType <: Number}(k2::NumType, theta2::NumType)
+#     function this_gamma_lk{NumType2 <: Number}(k1::NumType2, theta1::NumType2)
+#         digamma_k1 = digamma(k1)
+#         theta_ratio = (theta1 - theta2) / theta2
+#         shape_diff = k1 - k2
+#
+#         v = shape_diff * digamma_k1
+#         v += -lgamma(k1) + lgamma(k2)
+#         v += k2 * (log(theta2) - log(theta1))
+#         v += k1 * theta_ratio
+#
+#         d_k1 = shape_diff * trigamma(k1)
+#         d_k1 += theta_ratio
+#
+#         d_theta1 = -k2 / theta1
+#         d_theta1 += k1 / theta2
+#
+#         v, (d_k1, d_theta1)
+#     end
+# end
 
 
 @doc """
@@ -154,8 +154,10 @@ Returns:
   bivariate normal distribution, and returns the KL divergence
   and its derivatives
 """ ->
-function gen_isobvnormal_kl{NumType <: Number}(mean2::Vector{NumType}, var2::NumType)
-    function this_isobvnormal_kl{NumType2 <: Number}(mean1::Vector{NumType2}, var1::NumType2)
+function gen_isobvnormal_kl{NumType <: Number}(
+  mean2::Vector{NumType}, var2::NumType)
+    function this_isobvnormal_kl{NumType2 <: Number}(
+      mean1::Vector{NumType2}, var1::NumType2)
         diff_sq = (mean1[1] - mean2[1])^2 + (mean1[2] - mean2[2])^2
         v = var1 / var2 + diff_sq / 2var2 - 1 + log(var2 / var1)
 
@@ -176,15 +178,17 @@ Args:
 - cov2: the covariance matrix for the second normal distribution
 
 Returns:
-- a function the takes mean and variance parameters for the first normal 
+- a function the takes mean and variance parameters for the first normal
   distribution, and returns the KL divergence and its derivatives
 """ ->
-function gen_diagmvn_mvn_kl{NumType <: Number}(mean2::Vector{NumType}, cov2::Matrix{NumType})
+function gen_diagmvn_mvn_kl{NumType <: Number}(
+  mean2::Vector{NumType}, cov2::Matrix{NumType})
     const precision2 = cov2^-1
     const logdet_cov2 = logdet(cov2)
     const K = length(mean2)
 
-    function this_diagmvn_mvn_kl{NumType2 <: Number}(mean1::Vector{NumType2}, vars1::Vector{NumType2})
+    function this_diagmvn_mvn_kl{NumType2 <: Number}(
+      mean1::Vector{NumType2}, vars1::Vector{NumType2})
         const diag_precision2 = convert(Vector{NumType2}, diag(precision2))
         diff = mean2 - mean1
 
@@ -219,4 +223,3 @@ function gen_isobvnormal_flat_kl()
 end
 
 end
-
