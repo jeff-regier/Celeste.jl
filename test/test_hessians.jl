40a24245616db34011559a575e23509d6ec61953
Ryan Giordano <rgiordan@gmail.com>
2015-11-18 12:02:25 -0800

    Brightness using the multiply function
    Former-commit-id: ecf3cb24ca038a738e3de015eff13f31395f8d8a

--- test/test_hessians.jl
+++ test/test_hessians.jl
@@ -12,7 +12,75 @@ import WCS
 
 println("Running hessian tests.")
 
-include(joinpath(Pkg.dir("Celeste"), "src/SensitiveFloat.jl"))
+#include(joinpath(Pkg.dir("Celeste"), "src/SensitiveFloat.jl"))
+
+#function test_brightness_hessian()
+    blob, mp, three_bodies = gen_three_body_dataset();
+    kept_ids = [ ids_free.r1; ids_free.r2; ids_free.c1[:]; ids_free.c2[:] ];
+    omitted_ids = setdiff(1:length(ids_free), kept_ids);
+
+    # mp = deepcopy(mp_original);
+    # mp.vp = fill(mp_original.vp[1], 1);
+    # mp.patches = mp_original.patches[1, :];
+    # mp.active_sources = [1]
+    # mp.objids = fill(mp_original.objids[1], 1)
+    # mp.S = 1
+
+    transform = Transform.get_identity_transform(length(ids), mp.S);
+    ad_hess = zeros(Float64, length(kept_ids) * mp.S, length(kept_ids) * mp.S);
+    for i = 1:Ia
+        for b = [3,4,2,5,1]
+            # Write the brightness as an ordinary function of a ModelParams
+            # so we can use the objective function autodiff hessian logic.
+            function wrap_source_brightness{NumType <: Number}(
+                mp::ModelParams{NumType})
+              ret = zero_sensitive_float(CanonicalParams, NumType, mp.S);
+              for s=1:mp.S
+                sb = ElboDeriv.SourceBrightness(mp.vp[s])
+                ret.v = sb.E_l_a[b, i].v
+                ret.d[:, s] = sb.E_l_a[b, i].d
+                ret.hs[s][:, :] = sb.E_l_a[b, i].hs[1][:, :]
+              end
+              ret
+            end
+
+            bright = wrap_source_brightness(mp);
+            objective = OptimizeElbo.ObjectiveWrapperFunctions(
+              wrap_source_brightness, mp, transform, kept_ids, omitted_ids);
+            x = transform.vp_to_array(mp.vp, omitted_ids);
+
+            # Sanity check.
+            @test_approx_eq objective.f_value(x[:]) bright.v
+
+            # Compare the AD hessian with the exact hessian.
+            println("b $b i $i")
+            objective.f_ad_hessian!(x[:], ad_hess);
+            for s=1:mp.S
+              hess_ind = (1:length(kept_ids)) + (s - 1) * length(kept_ids)
+              @test_approx_eq ad_hess[hess_ind, hess_ind] bright.hs[kept_ids, kept_ids, s]
+            end
+
+            reduce(hcat, ind2sub(size(bright.hs[kept_ids, kept_ids]),
+                            find(abs(ad_hess[hess_ind, hess_ind][:] .-
+                                     bright.hs[kept_ids,kept_ids,1][:]) .> 1e-6)))
+        end
+    end
+    #
+    #
+    #
+    #         function wrap_source_brightness_3(mp)
+    #             sb = ElboDeriv.SourceBrightness(mp.vp[1])
+    #             ret = zero_sensitive_float(CanonicalParams, 3)
+    #             ret.v = sb.E_ll_a[b, i].v
+    #             ret.d[:, 1] = sb.E_ll_a[b, i].d
+    #             ret
+    #         end
+    #         test_by_finite_differences(wrap_source_brightness_3, mp0)
+    #     end
+    # end
+#end
+
+
 
 function test_multiply_sf()
   # Test for hessians.
@@ -105,76 +173,6 @@ function test_multiply_sf()
     @test_approx_eq(sf1.hs[s][:], hess[s_ind[s], s_ind[s]])
   end
 end
-
-
-
-#function test_brightness_hessian()
-    blob, mp, three_bodies = gen_three_body_dataset();
-    kept_ids = [ ids_free.r1; ids_free.r2; ids_free.c1[:]; ids_free.c2[:] ];
-    omitted_ids = setdiff(1:length(ids_free), kept_ids);
-
-    # mp = deepcopy(mp_original);
-    # mp.vp = fill(mp_original.vp[1], 1);
-    # mp.patches = mp_original.patches[1, :];
-    # mp.active_sources = [1]
-    # mp.objids = fill(mp_original.objids[1], 1)
-    # mp.S = 1
-
-    transform = Transform.get_identity_transform(length(ids), mp.S);
-    ad_hess = zeros(Float64, length(kept_ids) * mp.S, length(kept_ids) * mp.S);
-    for i = 1:Ia
-        for b = [3,4,2,5,1]
-            # Write the brightness as an ordinary function of a ModelParams
-            # so we can use the objective function autodiff hessian logic.
-            function wrap_source_brightness{NumType <: Number}(
-                mp::ModelParams{NumType})
-              ret = zero_sensitive_float(CanonicalParams, NumType, mp.S);
-              for s=1:mp.S
-                sb = ElboDeriv.SourceBrightness(mp.vp[s])
-                ret.v = sb.E_l_a[b, i].v
-                ret.d[:, s] = sb.E_l_a[b, i].d
-                ret.hs[:, :, s] = sb.E_l_a[b, i].hs[:, :, 1]
-              end
-              ret
-            end
-
-            bright = wrap_source_brightness(mp);
-            objective = OptimizeElbo.ObjectiveWrapperFunctions(
-              wrap_source_brightness, mp, transform, kept_ids, omitted_ids);
-            x = transform.vp_to_array(mp.vp, omitted_ids);
-
-            # Sanity check.
-            @test_approx_eq objective.f_value(x[:]) bright.v
-
-            # Compare the AD hessian with the exact hessian.
-            println("b $b i $i")
-            objective.f_ad_hessian!(x[:], ad_hess);
-            for s=1:mp.S
-              hess_ind = (1:length(kept_ids)) + (s - 1) * length(kept_ids)
-              @test_approx_eq ad_hess[hess_ind, hess_ind] bright.hs[kept_ids, kept_ids, s]
-            end
-
-            reduce(hcat, ind2sub(size(bright.hs[kept_ids, kept_ids]),
-                            find(abs(ad_hess[hess_ind, hess_ind][:] .-
-                                     bright.hs[kept_ids,kept_ids,1][:]) .> 1e-6)))
-        end
-    end
-    #
-    #
-    #
-    #         function wrap_source_brightness_3(mp)
-    #             sb = ElboDeriv.SourceBrightness(mp.vp[1])
-    #             ret = zero_sensitive_float(CanonicalParams, 3)
-    #             ret.v = sb.E_ll_a[b, i].v
-    #             ret.d[:, 1] = sb.E_ll_a[b, i].d
-    #             ret
-    #         end
-    #         test_by_finite_differences(wrap_source_brightness_3, mp0)
-    #     end
-    # end
-#end
-
-
 
 
 function test_set_hess()
