8f3a6bb4319a1996bbd494de8fafb85a0b4a74e6
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-12-18 11:22:36 -0800

    modified Utils again
    Former-commit-id: d1f3cdb90cfe1394874210f80a2d45d718285bb0

--- src/Util.jl
+++ src/Util.jl
@@ -21,8 +21,10 @@ function get_bvn_cov{NumType <: Number}(ab::NumType, angle::NumType, scale::NumT
     #    The 2x2 covariance matrix parameterized by the inputs.
 
     #@assert -pi/2 <= angle < pi/2
-    @assert 0 < convert(Float64, scale)
-    @assert 0 < convert(Float64, ab) <= 1.
+    if NumType <: AbstractFloat
+        @assert 0 < scale
+        @assert 0 < ab <= 1.
+    end
     cp, sp = cos(angle), sin(angle)
     R = [[cp -sp]; [sp cp]]  # rotates
     D = diagm([1., ab])  # shrinks the minor axis
