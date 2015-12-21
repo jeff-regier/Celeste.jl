abf0ce86d6237b8a4a2fbb6f6b97dd7b888012f6
Ryan Giordano <rgiordan@gmail.com>
2015-12-20 21:42:08 -0800

    Fixed bug in combine pixel sources

--- test/test_hessians.jl
+++ test/test_hessians.jl
@@ -16,7 +16,7 @@ println("Running hessian tests.")
 
 
 # Checking if the ELBO is different between the two versions.
-include("test/debug_with_master.jl")
+include("test/debug_with_master.jl"); include("src/ElboDeriv.jl")
 
 
 using Debug
@@ -101,6 +101,7 @@ end
 
 # Check the brightnesses.  Looks good.
 
+blob, mp, bodies, tiled_blob = gen_two_body_dataset();
 @time their_sbs = [Debug.ElboDeriv.SourceBrightness(mp.vp[s]) for s in 1:mp.S];
 @time our_sbs = ElboDeriv.load_source_brightnesses(mp, true);
 
@@ -114,7 +115,7 @@ end
 
 ##################################
 # Check a pixel accumlation.
-s = 1
+s = 2
 b = 1
 h = 10
 w = 10
@@ -145,7 +146,7 @@ function their_accumulate_pixel_stats()
       sbs[s], star_mcs, gal_mcs,
       mp.vp[s], s, s, m_pos, b, fs0m, fs1m, E_G, var_G, wcs_jacobian)
 
-  deepcopy(E_G)
+  deepcopy(E_G), deepcopy(var_G)
 end
 
 
@@ -162,16 +163,167 @@ function our_accumulate_pixel_stats()
   clear!(elbo_vars.E_G2);
 
   ElboDeriv.accumulate_source_brightness!(elbo_vars, mp, sbs, s, b);
-  deepcopy(elbo_vars.E_G);
+  deepcopy(elbo_vars.E_G), deepcopy(elbo_vars.E_G2)
 end
 
 
 
-their_E_G = their_accumulate_pixel_stats();
-our_E_G = our_accumulate_pixel_stats();
+their_E_G, their_var_G = their_accumulate_pixel_stats();
+our_E_G, our_E_G2 = our_accumulate_pixel_stats();
 
 @test_approx_eq their_E_G.v our_E_G.v
 @test_approx_eq their_E_G.d our_E_G.d
+
+@test_approx_eq their_var_G.v our_E_G2.v - (our_E_G.v ^ 2)
+
+################################
+# It might be in the accumulation of E_G2 across multiple sources.
+
+s = 1
+b = 1
+h = 10
+w = 10
+wcs_jacobian = Float64[1 0; 0 1]
+
+
+function their_expected_pixel_brightness()
+  blob, mp, bodies, tiled_blob = Debug.SampleData.gen_two_body_dataset();
+  tile = tiled_blob[b][1,1];
+
+  zsf = zero_sensitive_float;
+
+  star_mcs, gal_mcs = Debug.ElboDeriv.load_bvn_mixtures(mp, b);
+
+  accum = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+  fs0m = zsf(CelesteTypes.StarPosParams, Float64);
+  fs1m = zsf(CelesteTypes.GalaxyPosParams, Float64);
+  E_G = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+  var_G = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+
+  sbs = Debug.ElboDeriv.SourceBrightness{Float64}[
+    Debug.ElboDeriv.SourceBrightness(mp.vp[s]) for s in 1:mp.S];
+
+  m_pos = Float64[tile.h_range[h], tile.w_range[w]]
+
+  this_pixel = tile.pixels[h, w]
+  iota = Debug.ElboDeriv.expected_pixel_brightness!(
+    h, w, sbs, star_mcs, gal_mcs, tile, E_G, var_G,
+    mp, mp.active_sources, fs0m, fs1m,
+    include_epsilon=true);
+  deepcopy(E_G), deepcopy(var_G)
+end
+
+
+
+function our_expected_pixel_brightness()
+  blob, mp, bodies, tiled_blob = gen_two_body_dataset();
+  tile = tiled_blob[b][1,1];
+
+  star_mcs, gal_mcs = ElboDeriv.load_bvn_mixtures(mp, b);
+  sbs = ElboDeriv.load_source_brightnesses(mp, true);
+  elbo_vars = ElboDeriv.ElboIntermediateVariables(Float64, mp.S, mp.S);
+  ElboDeriv.populate_fsm_vecs!(
+    elbo_vars, mp, tile, h, w, sbs, gal_mcs, star_mcs);
+  clear!(elbo_vars.E_G);
+  clear!(elbo_vars.E_G2);
+
+  this_pixel = tile.pixels[h, w]
+  ElboDeriv.get_expected_pixel_brightness!(
+    elbo_vars, h, w, sbs, star_mcs, gal_mcs, tile,
+    mp, include_epsilon=true)
+
+  deepcopy(elbo_vars.E_G), deepcopy(elbo_vars.E_G2)
+end
+
+their_E_G, their_var_G = their_expected_pixel_brightness();
+our_E_G, their_E_G2 = our_expected_pixel_brightness();
+
+@test_approx_eq their_E_G.v our_E_G.v
+
+
+
+
+
+##################################
+# test accum_pixel_ret!
+
+s = 1
+b = 1
+h = 10
+w = 10
+wcs_jacobian = Float64[1 0; 0 1]
+
+
+function their_accum_pixel_ret()
+  blob, mp, bodies, tiled_blob = Debug.SampleData.gen_two_body_dataset();
+  tile = tiled_blob[b][1,1];
+
+  zsf = zero_sensitive_float;
+
+  star_mcs, gal_mcs = Debug.ElboDeriv.load_bvn_mixtures(mp, b);
+
+  accum = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+  fs0m = zsf(CelesteTypes.StarPosParams, Float64);
+  fs1m = zsf(CelesteTypes.GalaxyPosParams, Float64);
+  E_G = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+  var_G = zsf(CelesteTypes.CanonicalParams, Float64, mp.S);
+
+  sbs = Debug.ElboDeriv.SourceBrightness{Float64}[
+    Debug.ElboDeriv.SourceBrightness(mp.vp[s]) for s in 1:mp.S];
+
+  m_pos = Float64[tile.h_range[h], tile.w_range[w]]
+
+  this_pixel = tile.pixels[h, w]
+  iota = Debug.ElboDeriv.expected_pixel_brightness!(
+    h, w, sbs, star_mcs, gal_mcs, tile, E_G, var_G,
+    mp, mp.active_sources, fs0m, fs1m,
+    include_epsilon=true);
+
+  Debug.ElboDeriv.accum_pixel_ret!(
+    mp.active_sources, this_pixel, iota, E_G, var_G, accum);
+  deepcopy(accum)
+end
+
+
+
+function our_accum_pixel_ret()
+  blob, mp, bodies, tiled_blob = gen_two_body_dataset();
+  tile = tiled_blob[b][1,1];
+
+  star_mcs, gal_mcs = ElboDeriv.load_bvn_mixtures(mp, b);
+  sbs = ElboDeriv.load_source_brightnesses(mp, true);
+  elbo_vars = ElboDeriv.ElboIntermediateVariables(Float64, mp.S, mp.S);
+  ElboDeriv.populate_fsm_vecs!(
+    elbo_vars, mp, tile, h, w, sbs, gal_mcs, star_mcs);
+  clear!(elbo_vars.E_G);
+  clear!(elbo_vars.E_G2);
+
+  this_pixel = tile.pixels[h, w]
+  ElboDeriv.get_expected_pixel_brightness!(
+    elbo_vars, h, w, sbs, star_mcs, gal_mcs, tile,
+    mp, include_epsilon=true)
+
+
+  println(elbo_vars.elbo.v)
+  iota = tile.constant_background ? tile.iota : tile.iota_vec[h]
+  ElboDeriv.add_elbo_log_term!(elbo_vars, this_pixel, iota)
+  println("Log term: ", elbo_vars.elbo.v)
+  CelesteTypes.add_scaled_sfs!(elbo_vars.elbo, elbo_vars.E_G, scale=-iota)
+  println(elbo_vars.elbo.v)
+
+  deepcopy(elbo_vars.elbo)
+end
+
+their_accum = their_accum_pixel_ret();
+our_accum = our_accum_pixel_ret();
+
+
+
+# Different!
+their_accum.v
+our_accum.v
+
+
 
 
 
