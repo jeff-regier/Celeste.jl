fa2b4c5d01325f5ebf13fa345917488f587ea870
Ryan Giordano <rgiordan@gmail.com>
2016-01-19 17:26:41 -0800

    Clean tests
    Former-commit-id: 75e016f703361169d4c9921b857ca85f8b68d46d

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,10 +11,10 @@ import Synthetic
 
 include(joinpath(Pkg.dir("Celeste"), "test", "test_misc.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_kl.jl"))
+include(joinpath(Pkg.dir("Celeste"), "test", "test_sensitive_float.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_transforms.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_elbo_values.jl"))
-include(joinpath(Pkg.dir("Celeste"), "test", "test_derivs.jl"))
-include(joinpath(Pkg.dir("Celeste"), "test", "test_hessians.jl"))
+include(joinpath(Pkg.dir("Celeste"), "test", "test_derivatives.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_images.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_optimization.jl"))
 include(joinpath(Pkg.dir("Celeste"), "test", "test_distributed_celeste.jl"))
