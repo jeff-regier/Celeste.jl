e2c9a055f29fb5847d5db874aeda091744f1dc12
Ryan Giordano <rgiordan@gmail.com>
2016-01-21 12:04:34 -0800

    Restore benchmark script
    Former-commit-id: 71bae3df4783f0eb7658fd35352dc16d85042f53

--- bin/benchmark_elbo.jl
+++ bin/benchmark_elbo.jl
@@ -12,21 +12,21 @@ const dat_dir = joinpath(Pkg.dir("Celeste"), "dat")
 srand(1)
 println("Loading data.")
 
-S = 3
+S = 100
 blob, mp, body, tiled_blob =
   SampleData.gen_n_body_dataset(S, tile_width=10);
 
 println("Calculating ELBO.")
 
 # do a trial run first, so we don't profile/time compling the code
-@time ElboDeriv.elbo(tiled_blob, mp);
+@time ElboDeriv.elbo(tiled_blob, mp, calculate_derivs=false);
 
 # let's time it without any overhead from profiling
-@time ElboDeriv.elbo(tiled_blob, mp);
+@time ElboDeriv.elbo(tiled_blob, mp, calculate_derivs=false);
 
 # on a intel core2 Q6600 processor,
 # median runtime is consistently 27 seconds with Julia 0.3
 # median runtime is consistently 24 seconds with Julia 0.4
 Profile.init(10^8, 0.001)
-@profile elbo = ElboDeriv.elbo(tiled_blob, mp)
+@profile elbo = ElboDeriv.elbo(tiled_blob, mp, calculate_derivs=false)
 Profile.print(format=:flat)
