3075742f6b27af4c61f1809feb50eb1a06dbd996
Ryan Giordano <rgiordan@gmail.com>
2016-02-07 19:04:57 -0800

    Clean up process script, add a limit to local sources option

--- bin/staging/preprocess_image.jl
+++ bin/staging/preprocess_image.jl
@@ -24,14 +24,21 @@ blob =
 cat_df = SDSS.load_catalog_df(field_dir, run_num, camcol_num, field_num);
 cat_entries = SkyImages.convert_catalog_to_celeste(cat_df, blob);
 
-# TODO: change fit_psf to true when you're sure it works.
+# TODO: Fitting all the psfs in advance takes an annoyingly long time,
+# but initialize_celeste currently requires having the original blob, which
+# cannot be saved in an HDF5 file because the wcs object is a C pointer.
+# This could be refactored so that celeste can be re-initialized without the
+# WCS allowing us to fit the psfs at optimization time rather than initialization..
 tiled_blob, mp_all =
   ModelInit.initialize_celeste(blob, cat_entries,
-    tile_width=tile_width, fit_psf=true);
+    tile_width=tile_width, fit_psf=false);
+
+# TODO: You could use limit_to_object_data() together with trim_source_tiles()
+# to save only the data necessary for individual objects.
 
 # The blob cannot be saved because it has a pointer to a C++ object.
 JLD.save(string("$dat_dir/initialzed_celeste_",
-                "$(run_num)_$(camcol_num)_$(field_num)_$(tile_width)px.JLD"),
+                "$(run_num)_$(camcol_num)_$(field_num)_$(tile_width)px.jld"),
          Dict("tiled_blob" => tiled_blob,
               "mp_all" => mp_all,
               "cat_df" => cat_df,
