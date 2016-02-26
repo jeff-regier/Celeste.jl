675d9fae32fc306f8ddb736d86d0c576e61c58e2
Ryan Giordano <rgiordan@gmail.com>
2016-02-25 17:32:32 -0800

    Go back to original benchmark

--- bin/benchmark_elbo_with_hessian.jl
+++ bin/benchmark_elbo_with_hessian.jl
@@ -12,7 +12,7 @@ const dat_dir = joinpath(Pkg.dir("Celeste"), "dat")
 srand(1)
 println("Loading data.")
 
-S = 2
+S = 4
 blob, mp, body, tiled_blob =
   SampleData.gen_n_body_dataset(S, tile_width=10);
 
@@ -23,7 +23,7 @@ println("Calculating ELBO.")
 
 # let's time it without any overhead from profiling
 @time elbo = ElboDeriv.elbo(tiled_blob, mp, calculate_hessian=true);
-@time elbo = ElboDeriv.elbo_likelihood(tiled_blob, mp, calculate_hessian=true);
+@time elbo = ElboDeriv.elbo(tiled_blob, mp, calculate_hessian=true);
 
 Profile.init(10^8, 0.001)
 Profile.clear_malloc_data()
