b9bdb3d6380218e37a0b6b918295b55673f53b9d
Ryan Giordano <rgiordan@gmail.com>
2016-03-01 18:07:34 -0800

    Fix some stuff
    Former-commit-id: ab9a45ec338d1daa19b91abac84e69ab4b9f8ab7

--- bin/celeste_interactive.jl
+++ bin/celeste_interactive.jl
@@ -1,6 +1,17 @@
+# Run celeste interactively.
 
 using Celeste
 using CelesteTypes
 using DocOpt
 using JLD
 import SloanDigitalSkySurvey: SDSS
+
+include(joinpath(Pkg.dir("Celeste"), "src/api.jl"))
+
+dat_dir = joinpath(Pkg.dir("Celeste"), "test/data")
+
+run = "003900"
+camcol = "6"
+field = "0269"
+
+infer(dat_dir, run, camcol, field, "/tmp/", 1, 1000)
