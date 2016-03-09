21d145a3e874f44603e65fbc49729a4350533024
Ryan Giordano <rgiordan@gmail.com>
2016-03-09 00:05:48 -0800

    View and fit a single source

--- dev/celeste_interactive.jl
+++ dev/celeste_interactive.jl
@@ -11,8 +11,9 @@ using Celeste.Types
 import Celeste.SkyImages
 import Celeste.ModelInit
 import Celeste.OptimizeElbo
+import Celeste.ElboDeriv
 
-dat_dir = joinpath(Pkg.dir("Celeste"), "test/data")
+dir = joinpath(Pkg.dir("Celeste"), "test/data")
 
 run = "004263"
 camcol = "5"
@@ -22,36 +23,45 @@ field = "0117"
 # camcol = "6"
 # field = "0269"
 
-#Celeste.infer(dat_dir, run, camcol, field, "/tmp/", 4, 100)
+#Celeste.infer(dir, run, camcol, field, "/tmp/", 4, 100)
 
-images = SkyImages.load_sdss_blob(dat_dir, run, camcol, field);
+images = SkyImages.load_sdss_blob(dir, run, camcol, field);
 
 # load catalog and convert to Array of `CatalogEntry`s.
 cat_df = SDSS.load_catalog_df(dir, run, camcol, field);
 cat_entries = SkyImages.convert_catalog_to_celeste(cat_df, images);
 
-objid = "1237663784734359622"
-s = findfirst(objid, cat_df[:objid])
+# initialize tiled images and model parameters.  Don't fit the psf for now --
+# we just need the tile_sources from mp.
+tiled_blob, mp_all = ModelInit.initialize_celeste(images, cat_entries,
+                                                  tile_width=20,
+                                                  fit_psf=false);
 
-# initialize tiled images and model parameters
-tiled_blob, mp = ModelInit.initialize_celeste(images, cat_entries,
+#objid = "1237663784734359574" # Good
+objid = "1237663784734359622" # Bad
+s = findfirst(cat_df[:objid], objid)
+cat_df[s, :]
+
+relevant_sources = ModelInit.get_relevant_sources(mp_all, s);
+cat_entries_s = cat_entries[relevant_sources];
+tiled_blob, mp = ModelInit.initialize_celeste(images, cat_entries_s,
                                               tile_width=20,
                                               fit_psf=true);
-
-mp_s = deepcopy(mp);
-
-# Loop over sources in model
-println("Processing source $i, objid $(mp.objids[i])")
-
-mp_s.active_sources = [i]
+active_s = findfirst(mp.objids, objid)
+mp.active_sources = [ active_s ]
 
 # TODO: This is slow but would run much faster if you had run
 # limit_to_object_data() first.
-trimmed_tiled_blob = ModelInit.trim_source_tiles(i, mp_s, tiled_blob;
+trimmed_tiled_blob = ModelInit.trim_source_tiles(active_s, mp, tiled_blob;
                                                  noise_fraction=0.1);
+
+using PyPlot
+matshow(SkyImages.stitch_object_tiles(active_s, 3, mp, tiled_blob));
+PyPlot.colorbar()
+
 
 fit_time = time()
 iter_count, max_f, max_x, result =
-    OptimizeElbo.maximize_f(ElboDeriv.elbo, trimmed_tiled_blob, mp_s;
-                            verbose=true, max_iters=MAX_ITERS)
+    OptimizeElbo.maximize_f(ElboDeriv.elbo, trimmed_tiled_blob, mp;
+                            verbose=true, max_iters=50)
 fit_time = time() - fit_time
