acc0ffa893309c65c90985bbd7842354cc9a4da5
Ryan Giordano <rgiordan@gmail.com>
2016-03-09 12:52:08 -0800

    Incorporate changes to api
    Former-commit-id: 2697fb5aaebcbeded5b25da66c31f28c7654482b

--- dev/celeste_interactive.jl
+++ dev/celeste_interactive.jl
@@ -27,7 +27,11 @@ field = "0117"
 # camcol = "6"
 # field = "0269"
 
-#Celeste.infer(dir, run, camcol, field, "/tmp/", 4, 100)
+
+if false
+  Celeste.infer(dir, run, camcol, field, "/tmp/", 4, 100)
+end
+
 
 images = SkyImages.load_sdss_blob(dir, run, camcol, field);
 
@@ -41,33 +45,10 @@ tiled_blob, mp_all = ModelInit.initialize_celeste(images, cat_entries,
                                                   tile_width=20,
                                                   fit_psf=false);
 
-function initialze_objid(
-    objid::ASCIIString, mp_all::ModelParams{Float64},
-    cat_entries::Array{Celeste.Types.CatalogEntry},
-    images::Array{Celeste.Types.Image})
-
-  s = findfirst(mp_all.objids, objid)
-  #cat_df[s, :]
-
-  relevant_sources = ModelInit.get_relevant_sources(mp_all, s);
-  cat_entries_s = cat_entries[relevant_sources];
-  tiled_blob, mp = ModelInit.initialize_celeste(images, cat_entries_s,
-                                                tile_width=20,
-                                                fit_psf=true);
-  active_s = findfirst(mp.objids, objid)
-  mp.active_sources = [ active_s ]
-
-  # TODO: This is slow but would run much faster if you had run
-  # limit_to_object_data() first.
-  trimmed_tiled_blob = ModelInit.trim_source_tiles(active_s, mp, tiled_blob;
-                                                   noise_fraction=0.1);
-  trimmed_tiled_blob, mp, active_s, s
-end
 
 
 ## Look at fluxes
-
-MIN_FLUX = 1
+MAX_FLUX = 2
 flux_cols = [ symbol("psfflux_$b") for b in band_letters ]
 min_fluxes = Float64[ minimum([ cat_df[row, flux_col] for flux_col in flux_cols ])
                       for row in 1:size(cat_df, 1) ];
