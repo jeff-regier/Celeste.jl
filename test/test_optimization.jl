b42a18096762bccd3334c6e397b207506b29aca8
Ryan Giordano <rgiordan@gmail.com>
2016-03-10 16:23:18 -0800

    Use new stamp names
    Former-commit-id: cf747592212f01294b94b41caf82adc0ac66d010

--- test/test_optimization.jl
+++ test/test_optimization.jl
@@ -272,7 +272,7 @@ function test_bad_a_init()
     ce = CatalogEntry([7.2, 8.3], false, gal_color_mode, gal_color_mode,
             0.5, .7, pi/4, .5, "test")
 
-    blob0 = SkyImages.load_stamp_blob(datadir, "164.4311-39.0359");
+    blob0 = SkyImages.load_stamp_blob(datadir, "164.4311-39.0359_2kpsf");
     for b in 1:5
         blob0[b].H, blob0[b].W = 20, 23
         blob0[b].wcs = WCSUtils.wcs_id
@@ -321,8 +321,8 @@ end
 
 
 function test_real_stamp_optimization()
-    blob = SkyImages.load_stamp_blob(datadir, "5.0073-0.0739");
-    cat_entries = SkyImages.load_stamp_catalog(datadir, "s82-5.0073-0.0739", blob);
+    blob = SkyImages.load_stamp_blob(datadir, "5.0073-0.0739_2kpsf");
+    cat_entries = SkyImages.load_stamp_catalog(datadir, "s82-5.0073-0.0739_2kpsf", blob);
     bright(ce) = sum(ce.star_fluxes) > 3 || sum(ce.gal_fluxes) > 3
     cat_entries = filter(bright, cat_entries);
     inbounds(ce) = ce.pos[1] > -10. && ce.pos[2] > -10 &&
