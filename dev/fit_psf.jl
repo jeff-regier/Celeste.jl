3f8844928b833d401507a0ca9b919221f4a1e782
Ryan Giordano <rgiordan@gmail.com>
2016-03-16 14:57:31 -0700

    Basic transform
    Former-commit-id: 941e53213ce2c70f25a1e4e323fae56ef3b4c6fa

--- dev/fit_psf.jl
+++ dev/fit_psf.jl
@@ -1,20 +1,18 @@
 using Celeste
 using Celeste.Types
 using Celeste.SkyImages
-using Celeste.BivariateNormals
+using Celeste.PSF
+
 import Celeste.Util
 import Celeste.SensitiveFloats
 import Celeste.SDSSIO
 
-using SloanDigitalSkySurvey.PSF
 using PyPlot
 using Celeste.SensitiveFloats.SensitiveFloat
 
 using ForwardDiff
 
 using Base.Test
-
-include("src/PSF.jl")
 
 
 field_dir = joinpath(Pkg.dir("Celeste"), "test/data")
@@ -27,6 +25,7 @@ run_str = "004263"
 camcol_str = "5"
 field_str = "0117"
 b = 3
+K = 2
 
 psf_filename =
   @sprintf("%s/psField-%06d-%d-%04d.fit", field_dir, run_num, camcol_num, field_num)
@@ -34,13 +33,9 @@ psf_fits = FITSIO.FITS(psf_filename);
 raw_psf_comp = SDSSIO.read_psf(psf_fits, band_letters[b]);
 close(psf_fits)
 
-
 raw_psf = raw_psf_comp(500., 500.);
 #psf = SkyImages.fit_raw_psf_for_celeste(raw_psf);
 x_mat = PSF.get_x_matrix_from_psf(raw_psf);
-wcs_jacobian = eye(2);
-
-K = 2
 
 # Initialize params
 psf_params = zeros(length(Types.PsfParams), K)
@@ -52,99 +47,30 @@ for k=1:K
   psf_params[psf_ids.weight, k] = 1/ K
 end
 
-# For debugging
-NumType = Float64
 
 
+using Celeste.Transform.ParamBounds
+using Celeste.Transform.ParamBox
+using Celeste.Transform.DataTransform
 
-function pixel_value_wrapper_sf{NumType <: Number}(
-    psf_param_vec::Vector{NumType}, calculate_derivs::Bool)
+function get_psf_transform(psf_params::Matrix{Float64})
 
-  psf_params = reshape(psf_param_vec, length(PsfParams), 2)
+  bounds = Array(ParamBounds, size(psf_params, 2))
 
-  bvn_derivs = BivariateNormalDerivatives{NumType}(NumType);
-  log_pdf = SensitiveFloats.zero_sensitive_float(PsfParams, NumType, 1);
-  pdf = SensitiveFloats.zero_sensitive_float(PsfParams, NumType, 1);
+  # Note that, for numerical reasons, the bounds must be on the scale
+  # of reasonably meaningful changes.
+  for k in 1:K
+    bounds[k] = ParamBounds()
+    bounds[k][:mu] = fill(ParamBox(-5.0, 5.0, 1.0), 2)
+    bounds[k][:e_axis] = ParamBox[ ParamBox(0.1, 1.0, 1.0) ]
+    bounds[k][:e_angle] = ParamBox[ ParamBox(0.0, 4 * pi, 1.0) ]
+    bounds[k][:e_scale] = ParamBox[ ParamBox(0.25, Inf, 1.0) ]
 
-  pixel_value = SensitiveFloats.zero_sensitive_float(PsfParams, NumType, K);
-  squared_error = SensitiveFloats.zero_sensitive_float(PsfParams, NumType, K);
-
-  sigma_vec = Array(Matrix{NumType}, K);
-  sig_sf_vec = Array(GalaxySigmaDerivs{NumType}, K);
-
-  for k = 1:K
-    sigma_vec[k] = Util.get_bvn_cov(psf_params[psf_ids.e_axis, k],
-                                    psf_params[psf_ids.e_angle, k],
-                                    psf_params[psf_ids.e_scale, k])
-    sig_sf_vec[k] = GalaxySigmaDerivs(
-      psf_params[psf_ids.e_angle, k],
-      psf_params[psf_ids.e_axis, k],
-      psf_params[psf_ids.e_scale, k], sigma_vec[k], calculate_tensor=calculate_derivs);
-
+    # Note that the weights do not need to sum to one.
+    bounds[k][:e_weight] = ParamBox[ ParamBox(0.05, 2.0, 1.0) ]
   end
-
-  clear!(pixel_value)
-  evaluate_psf_pixel_fit!(
-      x, psf_params, sigma_vec, sig_sf_vec,
-      bvn_derivs, log_pdf, pdf, pixel_value, calculate_derivs)
-
-  pixel_value
-end
-
-function pixel_value_wrapper_value{NumType <: Number}(psf_param_vec::Vector{NumType})
-  pixel_value_wrapper_sf(psf_param_vec, false).v[1]
+  DataTransform(bounds, active_sources=collect(1:K), S=K)
 end
 
 
-# Pick a point for testing
-x_ind = 1508
-x = x_mat[x_ind]
-
-sigma_vec = Array(Matrix{Float64}, K);
-for k = 1:K
-  sigma_vec[k] = Util.get_bvn_cov(psf_params[psf_ids.e_axis, k],
-                                  psf_params[psf_ids.e_angle, k],
-                                  psf_params[psf_ids.e_scale, k])
-end
-
-psf_components = PsfComponent[
-  PsfComponent(psf_params[psf_ids.weight, k], psf_params[psf_ids.mu, k], sigma_vec[k])
-                for k = 1:K ];
-
-psf_rendered = get_psf_at_point(psf_components, rows=[ x[1] ], cols=[ x[2] ])[1];
-@test_approx_eq psf_rendered pixel_value_wrapper_value(psf_params[:])
-
-pixel_value = pixel_value_wrapper_sf(psf_params[:], true);
-
-ad_grad = ForwardDiff.gradient(pixel_value_wrapper_value, psf_params[:]);
-ad_hess = ForwardDiff.hessian(pixel_value_wrapper_value, psf_params[:]);
-
-@test_approx_eq ad_grad pixel_value.d[:]
-@test_approx_eq ad_hess[:] pixel_value.h[:]
-
-
-
-# Fewer pixels for quick testing.  Also, ForwardDiff.hessian runs into strange
-# problems on the whole image.
-keep_pixels = 20:30
-
-function evaluate_psf_fit_wrapper_sf{NumType <: Number}(
-      psf_param_vec::Vector{NumType}, calculate_derivs::Bool)
-  psf_params = reshape(psf_param_vec, length(PsfParams), 2)
-  evaluate_psf_fit(psf_params, raw_psf[keep_pixels, keep_pixels], calculate_derivs)
-end
-
-function evaluate_psf_fit_wrapper_value{NumType <: Number}(psf_param_vec::Vector{NumType})
-  squared_error = evaluate_psf_fit_wrapper_sf(psf_param_vec, false);
-  squared_error.v[1]
-end
-
-
-squared_error = evaluate_psf_fit_wrapper_sf(psf_params[:], true);
-squared_error.v[1]
-
-ad_grad = ForwardDiff.gradient(evaluate_psf_fit_wrapper_value, psf_params[:]);
-ad_hess = ForwardDiff.hessian(evaluate_psf_fit_wrapper_value, psf_params[:]);
-
-@test_approx_eq ad_grad squared_error.d[:]
-@test_approx_eq ad_hess[:] squared_error.h[:]
+psf_transform = get_psf_transform(psf_params)
