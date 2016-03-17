8d945de8dbec880625a5ceeb3d7dcf9926963633
Ryan Giordano <rgiordan@gmail.com>
2016-03-16 19:47:36 -0700

    Try the trust region code
    Former-commit-id: a1988e36d3343506601b3da57a6a80299667389a

--- dev/fit_psf.jl
+++ dev/fit_psf.jl
@@ -4,10 +4,9 @@ using Celeste.SkyImages
 using Celeste.PSF
 
 import Celeste.Util
-import Celeste.SensitiveFloats
 import Celeste.SDSSIO
 
-using Celeste.SensitiveFloats.SensitiveFloat
+using Celeste.SensitiveFloats
 
 using ForwardDiff
 
@@ -34,10 +33,10 @@ close(psf_fits)
 
 raw_psf = raw_psf_comp(500., 500.);
 
-psf_params = initialize_psf_params(K);
-psf_params_original = deepcopy(psf_params);
-psf_params_free = deepcopy(psf_params);
+psf_params_original = initialize_psf_params(K, for_test=true);
+psf_params = deepcopy(psf_params_original)
 psf_transform = PSF.get_psf_transform(psf_params);
+psf_params_free = unconstrain_psf_params(psf_params, psf_transform);
 psf_params_free_vec = wrap_psf_params(psf_params_free)[:];
 
 
@@ -46,24 +45,51 @@ function psf_fit_for_optim{NumType <: Number}(
 
   local sf_free = zero_sensitive_float(PsfParams, NumType, K);
   local psf_params_free = unwrap_psf_params(psf_params_free_vec)
-  local psf_params = unconstrain_psf_params(psf_params_free, psf_transform)
+  local psf_params = constrain_psf_params(psf_params_free, psf_transform)
   local sf = evaluate_psf_fit(psf_params, raw_psf, calculate_derivs);
-  transform_psf_sensitive_float!(psf_params, psf_transform, sf, sf_free, calculate_derivs)
+  if verbose
+    println(psf_params)
+  end
+  transform_psf_sensitive_float!(
+    psf_params, psf_transform, sf, sf_free, calculate_derivs)
 
   sf_free
 end
 
-
-function psf_fit_for_optim_val{NumType <: Number}(
-    psf_params_free_vec::Vector{NumType})
-
+function psf_fit_value{NumType <: Number}(psf_params_free_vec::Vector{NumType})
   psf_fit_for_optim(psf_params_free_vec, false).v[1]
 end
 
-sf_free = deepcopy(psf_fit_for_optim(psf_params_free_vec, true));
+function psf_fit_grad!(
+    psf_params_free_vec::Vector{Float64}, grad::Vector{Float64})
+  println(psf_params_free_vec)
+  grad[:] = psf_fit_for_optim(psf_params_free_vec, true).d[:]
+end
 
-ad_grad = ForwardDiff.gradient(psf_fit_for_optim_val, psf_params_free_vec);
-ad_hess = ForwardDiff.hessian(psf_fit_for_optim_val, psf_params_free_vec);
+function psf_fit_hess!(
+    psf_params_free_vec::Vector{Float64}, hess::Matrix{Float64})
+  hess[:] = psf_fit_for_optim(psf_params_free_vec, true).h
+end
 
-@test_approx_eq sf_free.d[:] ad_grad
-@test_approx_eq sf_free.h ad_hess
+d = Optim.TwiceDifferentiableFunction(
+  psf_fit_value, psf_fit_grad!, psf_fit_hess!)
+
+# Only include until this is merged with Optim.jl.
+include("src/newton_trust_region.jl")
+
+max_iters = 1000
+verbose = true
+rho_lower = 0.2
+
+nm_result = newton_tr(d,
+                      psf_params_free_vec,
+                      xtol = 0.0,
+                      ftol = 1e-9,
+                      grtol = 1e-9,
+                      iterations = max_iters,
+                      store_trace = verbose,
+                      show_trace = false,
+                      extended_trace = verbose,
+                      initial_delta=10.0,
+                      delta_hat=1e9,
+                      rho_lower = rho_lower)
