bf035029468a78f2e616ed6f4d25ef908d3ef3fb
Jeffrey Regier <jeff@stat.berkeley.edu>
2016-03-29 16:49:50 -0700

    scoring functions for fields and individual objects

--- bin/celeste.jl
+++ bin/celeste.jl
@@ -9,14 +9,16 @@ const DOC =
 Usage:
   celeste.jl [options] infer-box <ramin> <ramax> <decmin> <decmax> <outdir>
   celeste.jl [options] infer-field <run> <camcol> <field> <outdir>
+  celeste.jl [options] infer-object <run> <camcol> <field> <objid> <outdir>
   celeste.jl [options] score-field <run> <camcol> <field> <resultdir> <truthfile>
+  celeste.jl [options] score-object <run> <camcol> <field> <objid> <resultdir> <truthfile>
   celeste.jl -h | --help
   celeste.jl --version
 
 Options:
   -h, --help         Show this screen.
   --version          Show the version.
-  --logging=<LEVEL>   Level for the Logging package (OFF, DEBUG, INFO, WARNING, ERROR, or CRITICAL). [default: INFO]
+  --logging=<LEVEL>  Level for the Logging package (OFF, DEBUG, INFO, WARNING, ERROR, or CRITICAL). [default: INFO]
 
 The `infer-box` subcommand runs Celeste on all sources in the given
 RA/Dec range, using all available images.
@@ -37,14 +39,27 @@ function main()
         decmax = parse(Float64, args["<decmax>"])
         outdir = args["<outdir>"]
         Celeste.infer_box_nersc(ramin, ramax, decmin, decmax, outdir)
-    elseif args["infer-field"]
+    else
         run = parse(Int, args["<run>"])
         camcol = parse(Int, args["<camcol>"])
         field = parse(Int, args["<field>"])
-        outdir = args["<outdir>"]
-        Celeste.infer_field_nersc(run, camcol, field, outdir)
-    elseif args["score-field"]
-        Celeste.score_field_nersc(args["<resultdir>"], args["<truthfile>"])
+        if args["infer-field"]
+            outdir = args["<outdir>"]
+            Celeste.infer_field_nersc(run, camcol, field, outdir)
+        elseif args["infer-object"]
+            outdir = args["<outdir>"]
+            objid = args["<objid>"]
+            Celeste.infer_field_nersc(run, camcol, field, outdir; objid=objid)
+        elseif args["score-field"]
+            resultdir = args["<resultdir>"]
+            truthfile = args["<truthfile>"]
+            Celeste.score_field_nersc(run, camcol, field, resultdir, truthfile)
+        elseif args["score-object"]
+            objid = args["<objid>"]
+            resultdir = args["<resultdir>"]
+            truthfile = args["<truthfile>"]
+            Celeste.score_object_nersc(run, camcol, field, objid, resultdir, truthfile)
+        end
     end
 end
 
