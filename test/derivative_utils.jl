7e51227bca66fd74789292c3e88664655759aa00
Jeffrey Regier <jeff@stat.berkeley.edu>
2016-07-26 17:27:11 -0700

    require julia 0.5; delete dead code
    Former-commit-id: d489c5b1e580d32b8487ce156849e95e77d3395f

--- test/derivative_utils.jl
+++ test/derivative_utils.jl
@@ -1,33 +1,3 @@
-import Base.convert
-
-# function convert(FDType::Type{ForwardDiff.GradientNumber},
-#                  ea::ElboArgs{Float64})
-#     x = ea.vp[1]
-#     P = length(x)
-#     FDType = ForwardDiff.GradientNumber{length(ea.vp[1]), Float64}
-#
-#     fd_x = [ ForwardDiff.GradientNumber(x[i], zeros(Float64, P)...) for i=1:P ]
-#     convert(FDType, x[1])
-#
-#     vp_fd = convert(Array{Array{FDType, 1}, 1}, ea.vp[1])
-#     ea_fd = ElboArgs(vp_fd)
-# end
-#
-# function convert(FDType::Type{ForwardDiff.HessianNumber},
-#                  ea::ElboArgs{Float64})
-#     x = ea.vp[1]
-#     P = length(x)
-#     FDType = ForwardDiff.HessianNumber{length(ea.vp[1]), Float64}
-#
-#     fd_x = [ ForwardDiff.HessianNumber(x[i], zeros(Float64, P)...) for i=1:P ]
-#     convert(FDType, x[1])
-#
-#     vp_fd = convert(Array{Array{FDType, 1}, 1}, ea.vp[1])
-#     ea_fd = ElboArgs(vp_fd)
-# end
-
-
-# Maybe write it as a convert()?
 function forward_diff_model_params{T<:Number}(::Type{T}, ea0::ElboArgs{Float64})
     P = length(ea0.vp[1])
     vp = Vector{T}[zeros(T, P) for s=1:ea0.S]
