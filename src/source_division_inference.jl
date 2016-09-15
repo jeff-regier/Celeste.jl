9c938b43c6ca402958d868fed59ab9819e031552
Kiran Pamnany <kiran.pamnany@intel.com>
2016-09-14 22:09:10 -0700

    Remove StaticArrays, use serializing Garbo.jl (WIP)

--- src/source_division_inference.jl
+++ src/source_division_inference.jl
@@ -1,203 +1,20 @@
-using StaticArrays
 import WCS
 import Base.convert
 
-
-###########  flattened data types for use with Garbo #####
-
-type FlatCatalogEntry
-    pos::SVector{2,Float64}
-    is_star::Bool
-    star_fluxes::SVector{5,Float64}
-    gal_fluxes::SVector{5,Float64}
-    gal_frac_dev::Float64
-    gal_ab::Float64
-    gal_angle::Float64
-    gal_scale::Float64
-    objid::SVector{19,UInt8}
-    thing_id::Int
-end
-
-function convert(::Type{FlatCatalogEntry}, ce::CatalogEntry)
-    @assert length(ce.objid) == 19
-    FlatCatalogEntry(
-        ce.pos,
-        ce.is_star,
-        ce.star_fluxes,
-        ce.gal_fluxes,
-        ce.gal_frac_dev,
-        ce.gal_ab,
-        ce.gal_angle,
-        ce.gal_scale,
-        convert(Vector{UInt8}, ce.objid),
-        ce.thing_id)
-end
-
-function convert(::Type{CatalogEntry}, ce::FlatCatalogEntry)
-    CatalogEntry(
-        ce.pos,
-        ce.is_star,
-        ce.star_fluxes,
-        ce.gal_fluxes,
-        ce.gal_frac_dev,
-        ce.gal_ab,
-        ce.gal_angle,
-        ce.gal_scale,
-        convert(Vector{UInt8}, ce.objid),
-        ce.thing_id)
-end
-
-####
-
-immutable FlatRawPSF
-    rrows::SMatrix{2601,4,Float64}  # A matrix of flattened eigenimages.
-    rnrow::Int  # The number of rows in an eigenimage.
-    rncol::Int  # The number of columns in an eigenimage.
-    cmat::SVector{4,SMatrix{5,5,Float64}}  # The coefficients of the weight polynomial
-    nrow_b::Int
-    ncol_b::Int
-
-    function FlatRawPSF(rrows::Array{Float64, 2}, rnrow::Integer, rncol::Integer,
-                     cmat_raw::Array{Float64, 3}, nrow_b::Integer, ncol_b::Integer)
-        # rrows contains eigen images. Each eigen image is along the first
-        # dimension in a flattened form. Check that dimensions match up.
-        @assert size(rrows, 1) == rnrow * rncol
-
-        # The second dimension is the number of eigen images, which should
-        # match the number of coefficient arrays.
-        @assert size(rrows, 2) == size(cmat_raw, 3)
-
-        cmat2 = Matrix[cmat_raw[:,:,i] for i in 1:size(cmat_raw, 3)]
-
-        return new(rrows, Int(rnrow), Int(rncol), cmat2, Int(nrow_b), Int(ncol_b))
-    end
-end
-
-function convert(::Type{RawPSF}, psf::FlatRawPSF)
-    RawPSF(psf.rrows, psf.rnrow, psf.rncol, psf.cmat[1:psf.nrow_b, 1:psf.ncol_b])
-end
-
-function convert(::Type{FlatRawPSF}, psf::RawPSF)
-    cmat = zeros(5, 5, 4)
-    for k in 1:size(psf.cmat, 3)
-        cmat[size(psf.cmat, 1), size(psf.cmat, 2), k] = psf.cmat[:, :, k]
-    end
-    FlatRawPSF(psf.rrows, psf.rnrow, psf.rncol, cmat, psf.nrow_b, psf.ncol_b)
-end
-
-####
-
-immutable FlatPsfComponent
-    alphaBar::Float64
-    xiBar::SVector{2,Float64}
-    tauBar::SMatrix{2,2,Float64}
-    tauBarInv::SMatrix{2,2,Float64}
-    tauBarLd::Float64
-end
-
-function convert(::Type{FlatPsfComponent}, c::PsfComponent)
-    #xb = zeros(Float64, 2)
-    #copy!(xb, c.xiBar)
-    #tb = zeros(Float64, 2, 2)
-    #copy!(tb, c.tauBar)
-    #tbi = zeros(Float64, 2, 2)
-    #copy!(tbi, c.tauBarInv)
-    FlatPsfComponent(c.alphaBar, c.xiBar, c.tauBar, c.tauBarInv, c.tauBarLd)
-end
-
-function convert(::Type{PsfComponent}, c::FlatPsfComponent)
-    PsfComponent(c.alphaBar, c.xiBar, c.tauBar, c.tauBarInv, c.tauBarLd)
-end
-
-####
-
-immutable FlatImageTile
-    b::Int
-    h_range::UnitRange{Int}
-    w_range::UnitRange{Int}
-    pixels::SMatrix{20,20,Float32}
-    pixels_dims::Tuple{Int,Int}
-    epsilon_mat::SMatrix{20,20,Float32}
-    epsilon_mat_dims::Tuple{Int,Int}
-    iota_vec::SVector{20,Float32}
-    iota_vec_dims::Tuple{Int}
-end
-
-function convert(::Type{FlatImageTile}, it::ImageTile)
-    pm = zeros(Float32, 20, 20)
-    copy!(pm, it.pixels)
-    emm = zeros(Float32, 20, 20)
-    copy!(emm, it.epsilon_mat)
-    ivv = zeros(Float32, 20)
-    copy!(ivv, it.iota_vec)
-    #FlatImageTile(it.b, it.h_range, it.w_range, it.pixels, it.epsilon_mat, it.iota_vec)
-    FlatImageTile(it.b, it.h_range, it.w_range,
-            SMatrix{20,20,Float32}(pm), size(it.pixels),
-            SMatrix{20,20,Float32}(emm), size(it.epsilon_mat),
-            SVector{20,Float32}(ivv), size(it.iota_vec))
-end
-
-function convert(::Type{ImageTile}, it::FlatImageTile)
-    ImageTile(it.b, it.h_range, it.w_range, it.pixels, it.epsilon_mat, it.iota_vec)
-end
-
-####
-
-immutable FlatTiledImage
-    H::Int
-    W::Int
-    #tiles::SMatrix{100,200,FlatImageTile}
-    tiles::Matrix{FlatImageTile}
-    tiles_dims::Tuple{Int,Int}
-    tile_width::Int
-    b::Int
-    #wcs_header::SVector{10000,UInt8}
-    wcs_header::String
-    wcs_header_length::Int
-    psf::SVector{psf_K,FlatPsfComponent}
-    run_num::Int
-    camcol_num::Int
-    field_num::Int
-    #raw_psf_comp::FlatRawPSF
-    raw_psf_comp::RawPSF
-end
-
-function convert(::Type{FlatTiledImage}, img::TiledImage)
-    #@assert(size(img.tiles) == (100, 200))
-    sz = size(img.tiles)
-    @assert(sz[1] <= 105)
-    @assert(sz[2] <= 200)
-    wcs_header = WCS.to_header(img.wcs)
-    wcs_header_length = length(wcs_header)
-    @assert(wcs_header_length < 10_000)
-
-    FlatTiledImage(img.H, img.W, img.tiles, size(img.tiles), img.tile_width,
-                   img.b, wcs_header, wcs_header_length,
-                   img.psf, img.run_num, img.camcol_num, img.field_num,
-                   img.raw_psf_comp)
-end
-
-function convert(::Type{TiledImage}, img::FlatTiledImage)
-    wcs_array = WCS.from_header(img.wcs_header)
-    @assert(length(wcs_array) == 1)
-    wcs = wcs_array[1]
-    TiledImage(img.H, img.W, img.tiles, img.tile_width, img.b, wcs,
-               img.psf, img.run_num, img.camcol_num, img.field_num,
-               img.raw_psf_comp)
-end
-
-####
+# CatalogEntry size is 201 (pos:2, star_fluxes:5, gal_fluxes:5, objid:19)
+# RawPSF size is 84060 (rrows: 2601x4, cmat: 4x5x5)
+# PsfComponent size is 150 (xiBar: 2, tauBar:2x2, tauBarInv:2x2)
+# ImageTile size is 3296 (pixels: 20x20, epsilon_mat: 20x20, iota_vec: 20)
+# FlatTiledImage size is 69394456 (tiles: 105x200, wcs_header: 10000, psf: 2)
+# InferResult size is 299 (objid: 19, vs: 32)
 
 immutable InferResult
     thing_id::Int
-    objid::SVector{19,UInt8}
+    objid::String
     ra::Float64
     dec::Float64
-    vs::SVector{32,Float64}
+    vs::Vector{Float64}
 end
-
-
-###########################################################
 
 
 function fetch_catalog(rcf, stagedir)
@@ -211,7 +28,7 @@ function fetch_catalog(rcf, stagedir)
 end
 
 
-function in_box(entry::CatalogEntry, box::BoundingBox)
+@inline function in_box(entry::CatalogEntry, box::BoundingBox)
     box.ramin < entry.pos[1] < box.ramax &&
         box.decmin < entry.pos[2] < box.decmax
 end
@@ -233,21 +50,25 @@ function load_images(box, rcfs, stagedir)
     limages_lo, limages_hi = distribution(images, nodeid)
     nlocal_images = limages_hi[1] - limages_lo[1] + 1
     limages = access(images, limages_lo, limages_hi)
+    nputs(nodeid, "$nlocal_images local images to load")
 
     # we load images for the local distribution of the images global array only
     li = 1
     i_n = limages_lo[1]
     it_n = ((i_n - 1) * 100 * 200) + 1
     iwh_n = ((i_n - 1) * 10000) + 1
+    last_rcf_n = 0
     while i_n <= limages_hi[1]
         # there are 5 images per RCF so an RCF's images  might be split across nodes
         # (which is not good, but the fix is TODO); figure out which RCF corresponds
         # to the images we want and load it
         rcf_n, rcf_n_b = divrem(i_n, 5)
         rcf_n = rcf_n + 1
-        rcf = rcfs[rcf_n]
-        raw_images = SDSSIO.load_field_images(rcf, stagedir)
-        @assert(length(raw_images) == 5)
+        if rcf_n != last_rcf_n
+            rcf = rcfs[rcf_n]
+            raw_images = SDSSIO.load_field_images(rcf, stagedir)
+            @assert(length(raw_images) == 5)
+        end
         for b = rcf_n_b:5
             limages[li] = convert(FlatTiledImage, TiledImage(raw_images[b]))
 
@@ -269,9 +90,9 @@ function load_images(box, rcfs, stagedir)
         end
         i_n = i_n + (5 - rcf_n_b + 1)
     end
-    Garbo.flush(images)
-    Garbo.flush(images_tiles)
-    Garbo.flush(images_wcs_headers)
+    flush(images)
+    flush(images_tiles)
+    flush(images_wcs_headers)
     sync()
 
     images, images_tiles, images_wcs_headers
@@ -308,8 +129,8 @@ function load_offsets(box, rcfs, stagedir)
         lcatalog_offset[i] = length(local_catalog)
         ltask_offset[i] = length(local_tasks)
     end
-    Garbo.flush(catalog_offset)
-    Garbo.flush(task_offset)
+    flush(catalog_offset)
+    flush(task_offset)
     sync()
 
     # folds right, converting each field's count to offsets in # `catalog`
@@ -338,8 +159,8 @@ function load_offsets(box, rcfs, stagedir)
             end
         end
         # sync to ensure orderly progression
-        Garbo.flush(catalog_offset)
-        Garbo.flush(task_offset)
+        flush(catalog_offset)
+        flush(task_offset)
         sync()
     end
 
@@ -408,9 +229,10 @@ function invert_rcf_array(rcfs)
     max_field = 1
     for rcf in rcfs
         max_run = max(max_run, rcf.run)
-        max_camcol = max(max_run, rcf.camcol)
-        max_field = max(max_run, rcf.field)
+        max_camcol = max(max_camcol, rcf.camcol)
+        max_field = max(max_field, rcf.field)
     end
+    nputs(nodeid, "max_run: $max_run, max_camcol: $max_camcol, max_field: $max_field")
 
     rcf_to_index = zeros(Int64, max_run, max_camcol, max_field)
 
@@ -510,7 +332,7 @@ function optimize_sources(images, catalog, tasks, catalog_offset, task_offset,
     end
 
     # create Dtree and get the initial allocation
-    dt, isparent = DtreeScheduler(num_work_items, 0.4)
+    dt, isparent = Dtree(num_work_items, 0.4)
     ni, (ci, li) = initwork(dt)
     wi = get(tasks, [ci], [li])
     ci = 1
@@ -551,25 +373,25 @@ function divide_sources_and_infer(
 
     # loads 25TB from disk for SDSS
     tic()
-    images, images_tiles, images_wcs_headers = load_images(box, rcfs, stagedir)
+    @time images, images_tiles, images_wcs_headers = load_images(box, rcfs, stagedir)
     timing.read_img = toq()
 
-    catalog_offset, task_offset = load_offsets(box, rcfs, stagedir)
+    @time catalog_offset, task_offset = load_offsets(box, rcfs, stagedir)
 
     # loads 4TB from disk for SDSS
     tic()
-    catalog, tasks = load_catalog(box, rcfs, catalog_offset, task_offset, stagedir)
+    @time catalog, tasks = load_catalog(box, rcfs, catalog_offset, task_offset, stagedir)
     timing.read_photoobj = toq()
 
     # create map from run, camcol, field to index into RCF array
-    rcf_to_index = invert_rcf_array(rcfs)
+    @time rcf_to_index = invert_rcf_array(rcfs)
 
     # inference results are written here
     results = Garray(InferResult, length(catalog))
 
     # optimization -- little disk access, cpu intensive
     timing.num_srcs = length(tasks)
-    optimize_sources(images, catalog, tasks, catalog_offset, task_offset,
+    @time optimize_sources(images, catalog, tasks, catalog_offset, task_offset,
             rcf_to_index, stagedir, results, timing)
 end
 
