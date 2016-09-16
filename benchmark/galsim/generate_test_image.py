223404cf755f0efdae0e644fa6cf01066a134932
Steve Howard <dev@gostevehoward.com>
2016-09-15 22:32:49 -0700

    generate_test_image.py: simplify psf; generate images from truth csv
    First, I've removed all the fancy PSF and other nonsense that isn't really
    important at this stage.
    
    Second, I've changed this script around. Instead of writing a grid of randomized
    images along with a truth CSV, it now *reads* a list of test cases (in the form
    of "true" galaxy and noise parameters) from a truth CSV, and writes a series of
    images, one for each row. The julia script can then read in this CSV, read the
    corresponding image for each row, run Celeste, and compare its output to the CSV
    values.

--- benchmark/galsim/generate_test_image.py
+++ benchmark/galsim/generate_test_image.py
@@ -9,145 +9,72 @@ _logger = logging.getLogger(__name__)
 
 TRUTH_COLUMNS = ['left', 'bottom', 'angle_radians', 'ellipticity', 'world_center_x', 'world_center_y']
 
-# Galaxy parameters
-GALAXY_RADIUS=0.85 # arcsec
-GALAXY_FLUX = 1.e5 # total counts in image
-GAL_ELLIP_MIN = 0.2             # Minimum value of minor/major axis ratio
-
-# PSF parameters
-ATMOS_FWHM=2.1         # arcsec
-ATMOS_E = 0.13         # 
-ATMOS_BETA = 0.81      # radians
-OPT_DEFOCUS=0.53       # wavelengths
-OPT_A1=-0.29           # wavelengths
-OPT_A2=0.12            # wavelengths
-OPT_C1=0.64            # wavelengths
-OPT_C2=-0.33           # wavelengths
-OPT_OBSCURATION=0.3    # linear scale size of secondary mirror obscuration
-LAM = 800              # nm    NB: don't use lambda - that's a reserved word.
-TEL_DIAM = 4.          # meters
-
-# Other parameters
-PIXEL_SCALE = 0.75  # arcsec / pixel
+PIXEL_SCALE = 0.75 # arcsec / pixel
 SKY_LEVEL = 0. # TODO
+SHIFT_RADIUS_ARCSEC = PIXEL_SCALE
+PSF_SIGMA_ARCSEC = PIXEL_SCALE # so that PSF sigma = 1 pixel
+STAMP_SIZE_PX = 48
 
-SHIFT_RADIUS = 5.0              # arcsec
+RANDOM_SEED = 1234
 
-NX_TILES = 1
-NY_TILES = 1
-STAMP_XSIZE = 48
-STAMP_YSIZE = 48
-
-RANDOM_SEED = 6
-
-def make_galaxy():
-    return galsim.Exponential(scale_radius=GALAXY_RADIUS, flux=GALAXY_FLUX)
-
-def make_atmospheric_psf():
-    atmos = galsim.Kolmogorov(fwhm=ATMOS_FWHM)
-    return atmos.shear(e=ATMOS_E, beta=ATMOS_BETA*galsim.radians)
-
-def make_optical_psf():
-    # The first argument of OpticalPSF below is lambda/diam (wavelength of light / telescope
-    # diameter), which needs to be in the same units used to specify the image scale.  We are using
-    # arcsec for that, so we have to self-consistently use arcsec here, using the following
-    # calculation:
-    lam_over_diam = LAM * 1.e-9 / TEL_DIAM # radians
-    lam_over_diam *= 206265  # arcsec
-    _logger.debug('Calculated lambda over diam = %f arcsec', lam_over_diam)
-    return galsim.OpticalPSF(
-        lam_over_diam, 
-        defocus=OPT_DEFOCUS,
-        coma1=OPT_C1,
-        coma2=OPT_C2,
-        astig1=OPT_A1,
-        astig2=OPT_A2,
-        obscuration=OPT_OBSCURATION,
-    )
+def make_galaxy(half_light_radius_arcsec, flux_counts):
+    return galsim.Exponential(half_light_radius=half_light_radius_arcsec, flux=flux_counts)
 
 def make_psf():
-    return galsim.Convolve([make_atmospheric_psf(), make_optical_psf()])
+    return galsim.Gaussian(flux=1., sigma=PSF_SIGMA_ARCSEC)
 
-def get_stamp_subimage(full_image, x_index, y_index):
-    bounds = galsim.BoundsI(
-        x_index * STAMP_XSIZE + 1,
-        (x_index + 1) * STAMP_XSIZE - 1,
-        y_index * STAMP_YSIZE + 1,
-        (y_index + 1) * STAMP_YSIZE - 1,
-    )
-    return full_image[bounds]
+def apply_shear(galaxy, angle_degrees, minor_major_axis_ratio):
+    return galaxy.shear(q=minor_major_axis_ratio, beta=angle_degrees * galsim.degrees)
 
-def apply_shear(galaxy, uniform_deviate):
-    beta = uniform_deviate() * 2. * math.pi * galsim.radians
-    ellip = uniform_deviate() * (1 - GAL_ELLIP_MIN) + GAL_ELLIP_MIN
-    return galaxy.shear(q=ellip, beta=beta), beta, ellip
+def apply_shift(galaxy, shift_x_world_coords, shift_y_world_coords):
+    return galaxy.shift(shift_x_world_coords, shift_y_world_coords)
 
-def apply_shift(galaxy, uniform_deviate):
-    rsq = 2 * SHIFT_RADIUS**2
-    while (rsq > SHIFT_RADIUS**2):
-        dx = (10 * uniform_deviate() - 5) * SHIFT_RADIUS
-        dy = (2 * uniform_deviate() - 1) * SHIFT_RADIUS
-        rsq = dx**2 + dy**2
-    return galaxy.shift(dx, dy), dx, dy
-
-def add_noise(image, uniform_deviate):
-    noise = galsim.PoissonNoise(uniform_deviate, sky_level=SKY_LEVEL)
+def add_noise(image, sky_level, uniform_deviate):
+    noise = galsim.PoissonNoise(uniform_deviate, sky_level=sky_level)
     image.addNoise(noise)
 
-def write_truth(file_name, rows):
-    with open(file_name, 'w') as truth_stream:
-        writer = csv.DictWriter(truth_stream, TRUTH_COLUMNS)
-        writer.writeheader()
-        for row in rows:
-            writer.writerow(row)
+def read_truth(file_name):
+    with open(file_name) as truth_stream:
+        reader = csv.DictReader(truth_stream)
+        for row in reader:
+            yield row
 
 def main():
     logging.basicConfig(format="%(message)s", level=logging.INFO)
 
-    gal = make_galaxy()
-    psf = make_psf()
+    truth_file_name = 'galsim_truth.csv'
+    _logger.info('Reading %s', truth_file_name)
+    for index, truth_dict in enumerate(read_truth(truth_file_name)):
+        uniform_deviate = galsim.UniformDeviate(RANDOM_SEED + index)
+        image = galsim.ImageF(STAMP_SIZE_PX, STAMP_SIZE_PX, scale=PIXEL_SCALE)
+        galaxy = make_galaxy(
+            float(truth_dict['half_light_radius_arcsec']),
+            float(truth_dict['flux_counts']),
+        )
+        galaxy = apply_shear(
+            galaxy,
+            float(truth_dict['angle_degrees']),
+            float(truth_dict['minor_major_axis_ratio']),
+        )
+        galaxy = apply_shift(
+            galaxy,
+            float(truth_dict['world_center_x']) - STAMP_SIZE_PX * PIXEL_SCALE / 2.0,
+            float(truth_dict['world_center_y']) - STAMP_SIZE_PX * PIXEL_SCALE / 2.0,
+        )
+        psf = make_psf()
+        final_light_source = galsim.Convolve([galaxy, psf])
+        final_light_source.drawImage(image)
+        if truth_dict['add_noise'] == '1':
+            add_noise(image, float(truth_dict['sky_level']), uniform_deviate)
 
-    gal_image = galsim.ImageF(
-        STAMP_XSIZE * NX_TILES - 1,
-        STAMP_YSIZE * NY_TILES - 1,
-        scale=PIXEL_SCALE,
-    )
+        if not os.path.exists('output'):
+            os.mkdir('output')
 
-    _logger.info('Generating %d galaxy images', NX_TILES * NY_TILES)
-    k = 0
-    truth_rows = []
-    for iy in range(NY_TILES):
-        for ix in range(NX_TILES):
-            uniform_deviate = galsim.UniformDeviate(RANDOM_SEED + k + 1)
-            k += 1
+        image_file_name = os.path.join('output', 'galsim_test_image_{}.fits'.format(index))
+        image.write(image_file_name)
+        _logger.info('Wrote image to %r', image_file_name)
 
-            this_galaxy, beta, ellipticity = apply_shear(gal, uniform_deviate)
-            this_galaxy, shift_x, shift_y = apply_shift(this_galaxy, uniform_deviate)
-
-            stamp_image = get_stamp_subimage(gal_image, ix, iy)
-            final_gal = this_galaxy #galsim.Convolve([psf, this_galaxy])
-            final_gal.drawImage(stamp_image)
-            add_noise(stamp_image, uniform_deviate)
-
-            truth_rows.append({
-                'left': ix * STAMP_XSIZE,
-                'bottom': iy * STAMP_YSIZE,
-                'angle_radians': beta.rad(),
-                'ellipticity': ellipticity,
-                'world_center_x': (STAMP_XSIZE / 2 + shift_x) * PIXEL_SCALE,
-                'world_center_y': (STAMP_YSIZE / 2 + shift_y) * PIXEL_SCALE,
-            })
-
-    if not os.path.exists('output'):
-        os.mkdir('output')
-
-    image_file_name = os.path.join('output', 'galsim_test_image.fits')
-    gal_image.write(image_file_name)
-    _logger.info('Wrote image to %r', image_file_name)
-
-    truth_file_name = os.path.join('output', 'galsim_truth.csv')
-    write_truth(truth_file_name, truth_rows)
-    _logger.info('Wrote truth values to %r', truth_file_name)
+    _logger.info('Done.')
 
 if __name__ == "__main__":
     main()
