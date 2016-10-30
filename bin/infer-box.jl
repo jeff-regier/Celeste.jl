1c2de4fb5e3e80321a167aca0913fd9aebaf20d9
Jeffrey Regier <regier@gmail.com>
2016-10-30 14:23:20 -0700

    Removed DocOpt because it takes 4 second to load everytime; new top-level script is , now that the other subcommands of celeste.jl are in the benchmark directory

---
+++ bin/infer-box.jl
@@ -0,0 +1,23 @@
+#!/usr/bin/env julia
+
+import Celeste.ParallelRun: BoundingBox, infer_box
+import Celeste.SDSSIO: RunCamcolField
+
+
+const usage_info =
+"""
+Usage:
+  celeste.jl [options] infer-box <ramin> <ramax> <decmin> <decmax>
+"""
+
+const stagedir = ENV["CELESTE_STAGE_DIR"]
+
+if length(ARGS) != 4
+    println(usage_info)
+else
+    box = BoundingBox(ARGS...)
+    # Output gets written to the top level of the staging directory.
+    # We may want to modify that in the future by changing the third argument.
+    infer_box(box, stagedir, stagedir)
+end
+
