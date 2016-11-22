5698e61eafc3eef5378a59a682e1e653e00b5522
Jeffrey Regier <regier@gmail.com>
2016-11-21 16:58:35 -0800

    added script that prints all the quarter square degree bounding boxes imaged by SDSS; output can be piped to infer-box and estimate-box-runtime

---
+++ staging/list_quarters.jl
@@ -0,0 +1,13 @@
+#!/usr/bin/env julia
+
+# Lists all of the quarters of square degrees around the area
+# imaged by SDSS. Output in this format can be piped to either infer-box.jl
+# or estimate-box-runtime.jl, e.g.
+# 
+#   ./list_quarters.jl | sort -R | xargs -n 4 estimate-box-runtime.jl
+
+for ra in 0:.5:380, dec in -30:.5:90
+    println("$(ra - 0.5) $ra $(dec - 0.5) $dec")
+end
+
+
