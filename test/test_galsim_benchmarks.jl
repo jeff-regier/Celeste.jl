9712864a4ca4615e8dc0ba4f7190d372919b7dc8
gostevehoward <dev@gostevehoward.com>
2016-12-21 11:22:09 -0800

    fix galsim "unit test" and add some cases (#488)
    sadly this unit test was implemented in a completely broken way so it wasn't really checking
    parameter values. this fixes that. i've also added a few additional cases and commented out the two
    that are currently broken due to issue #482.

--- test/test_galsim_benchmarks.jl
+++ test/test_galsim_benchmarks.jl
@@ -4,9 +4,17 @@ using DataFrames
 
 import Celeste: GalsimBenchmark
 
+GALSIM_CASES_EXERCISED = [
+    #"simple_star", "probability of galaxy" is wrong (#482)
+    "star_with_noise",
+    "angle_and_axis_ratio_1",
+    "galaxy_with_all",
+    #"galaxy_with_noise", half-light radius and brightness are wrong (#482)
+]
+
 @testset "GalSim benchmark tests" begin
     results = GalsimBenchmark.main(
-        test_case_names=["simple_star", "angle_and_axis_ratio_1"],
+        test_case_names=GALSIM_CASES_EXERCISED,
         print_fn=x -> 0,
     )
     for row_index in 1:size(results, 1)
@@ -14,12 +22,15 @@ import Celeste: GalsimBenchmark
         if isna(row[1, :ground_truth])
             continue
         end
+        if row[1, :field] == "Probability of galaxy"
+            maximum_error = 0.1
+        elseif row[1, :field] == "Angle (degrees)"
+            maximum_error = 5
+        else
+            maximum_error = 0.1 * max(row[1, :ground_truth])
+        end
         for result_column in [:single_inferred, :joint_inferred]
-            @test_approx_eq_eps(
-                results[1, :ground_truth],
-                results[1, result_column],
-                0.05 * results[1, :ground_truth], # maximum permissible error
-            )
+            @test_approx_eq_eps(row[1, :ground_truth], row[1, result_column], maximum_error)
         end
     end
 end
