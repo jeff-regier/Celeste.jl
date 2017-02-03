3d6f01aa5922d39bb4f1abd5d6e794e6304a234d
Andreas Noack <andreasnoackjensen@gmail.com>
2017-02-03 06:38:13 -0500

    Adjust tolerance in test_eda to avoid test failure and use isapprox instead of deprecated @test_approx_eq_eps macro

--- test/test_eda.jl
+++ test/test_eda.jl
@@ -20,8 +20,8 @@ function test_render()
     img_1[isnan(img_1)] = 0
     img_2[isnan(img_2)] = 0
     img_12[isnan(img_12)] = 0
-    @test_approx_eq_eps(maximum(abs(img_12 - img_1 - img_2)), 0.0, 1e-15)
-    
+    @test isapprox(maximum(abs(img_12 - img_1 - img_2)), 0.0, atol = 1e-12)
+
     ea_fft = DeterministicVIImagePSF.ElboArgs(
         images, deepcopy(ea.vp), patches, collect(1:ea.S), psf_K=1);
     psf_image_mat = Matrix{Float64}[
@@ -37,7 +37,7 @@ function test_render()
     fft_img_1[isnan(fft_img_1)] = 0
     fft_img_2[isnan(fft_img_2)] = 0
     fft_img_12[isnan(fft_img_12)] = 0
-    @test_approx_eq_eps(maximum(abs(fft_img_12 - fft_img_1 - fft_img_2)), 0.0, 1e-15)
+    @test isapprox(maximum(abs(fft_img_12 - fft_img_1 - fft_img_2)), 0.0, atol = 1e-12)
 
     full_img_12 = CelesteEDA.render_sources(ea, [1, 2], n, include_epsilon=true, include_iota=true);
     full_fft_img_12 = CelesteEDA.render_sources_fft(ea_fft, fsm_mat, [1, 2], n, include_epsilon=true, include_iota=true);
@@ -45,7 +45,7 @@ function test_render()
     full_img_12[isnan(full_img_12)] = 0
     full_fft_img_12[isnan(full_fft_img_12)] = 0
     orig_img_12[isnan(orig_img_12)] = 0
-    
+
     # Check that the two images are roughly the same, and roughly the same as the original..
     @test median(abs(full_fft_img_12 - full_img_12)) / maximum(abs(full_img_12)) < 0.01
     @test median(abs(full_fft_img_12 - orig_img_12)) / maximum(abs(orig_img_12)) < 0.02
