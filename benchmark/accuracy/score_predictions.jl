a891ce96ac111be1b3305a9d30ab91b82414b160
Steve Howard <dev@gostevehoward.com>
2017-02-22 14:39:56 -0800

    direct support for scoring just one prediction catalog

--- benchmark/accuracy/score_predictions.jl
+++ benchmark/accuracy/score_predictions.jl
@@ -6,14 +6,23 @@ import Celeste.AccuracyBenchmark
 import Celeste.Infer
 import Celeste.ParallelRun
 
-if length(ARGS) != 3
-    println("Usage: score_predictions.jl <ground truth> <prediction> <prediction>")
+if !(2 <= length(ARGS) <= 3)
+    println("Usage: score_predictions.jl <ground truth> <predictions> [predictions]")
     println("Where each argument is a catalog CSV")
 end
 
+have_second_predictions = (length(ARGS) == 3)
+
 truth = AccuracyBenchmark.read_catalog(ARGS[1])
 first_predictions = AccuracyBenchmark.read_catalog(ARGS[2])
-second_predictions = AccuracyBenchmark.read_catalog(ARGS[3])
+if have_second_predictions
+    second_predictions = AccuracyBenchmark.read_catalog(ARGS[3])
+else
+    second_predictions = first_predictions
+end
 
 scores = AccuracyBenchmark.score_predictions(truth, [first_predictions, second_predictions])
+if !have_second_predicitons
+    scores = scores[:, [:N, :first, :field, :source_type]]
+end
 println(repr(scores))
