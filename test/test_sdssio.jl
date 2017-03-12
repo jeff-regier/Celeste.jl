575e3cd9a1a82f1432d07abe7380ae42707248cd
Jeffrey Regier <regier@gmail.com>
2017-03-12 15:56:50 -0700

    fixed sdssio test

--- test/test_sdssio.jl
+++ test/test_sdssio.jl
@@ -1,30 +1,33 @@
+import Celeste.SDSSIO: SkyIntensity
+
 function test_interp_sky()
-    data = [1.  2.  3.  4.;
+    small_sky = [1.  2.  3.  4.;
             5.  6.  7.  8.;
             9. 10. 11. 12]
-    xcoords = [0.1, 2.5]
-    ycoords = [0.5, 2.5, 4.]
-    result = SDSSIO.interp_sky(data, xcoords, ycoords)
-    @test size(result) == (2, 3)
-    @test result[1, 1] ≈ 1.0
-    @test result[2, 1] ≈ 7.0
-    @test result[1, 2] ≈ 2.5
-    @test result[2, 2] ≈ 8.5
-    @test result[1, 3] ≈ 4.0
-    @test result[2, 3] ≈ 10.0
+    sky_x = [0.1, 2.5]
+    sky_y = [0.5, 2.5, 4.]
+    sky = SkyIntensity(small_sky, sky_x, sky_y)
+    @test sky[1, 1] ≈ 1.0
+    @test sky[2, 1] ≈ 7.0
+    @test sky[1, 2] ≈ 2.5
+    @test sky[2, 2] ≈ 8.5
+    @test sky[1, 3] ≈ 4.0
+    @test sky[2, 3] ≈ 10.0
 end
 
 # test coordinates out of bounds
 function test_interp_sky_oob()
-    data = [1.  2.  3.  4.;
+    small_sky = [1.  2.  3.  4.;
             5.  6.  7.  8.;
             9. 10. 11. 12]
-    xcoords = [-5.0, 4.0]
-    ycoords = [-4.0, 5.0]
+    sky_x = [-5.0, 4.0]
+    sky_y = [-4.0, 5.0]
+    sky = SkyIntensity(small_sky, sky_x, sky_y)
 
-    result = SDSSIO.interp_sky(data, xcoords, ycoords)
-    @test result ≈ [1.0 4.0;
-                    9.0 12.0]
+    @test sky[1,1] == 1.0
+    @test sky[1,2] == 4.0
+    @test sky[2,1] == 9.0
+    @test sky[2,2] == 12.0
 end
 
 # test that read_photoobj handles missing table extensions.
