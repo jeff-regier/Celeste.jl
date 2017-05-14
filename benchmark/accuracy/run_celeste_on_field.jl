ed2b235a2e2f9bb97f515269e4a7ee904ac7e4c5
Steve Howard <dev@gostevehoward.com>
2017-05-14 16:39:16 -0700

    write celeste jld catalog in the standard name format

--- benchmark/accuracy/run_celeste_on_field.jl
+++ benchmark/accuracy/run_celeste_on_field.jl
@@ -115,9 +115,18 @@ results_df = AccuracyBenchmark.celeste_to_df(results)
 
 # save results in both JLD and CSV format
 
-jld_filename = joinpath(OUTPUT_DIRECTORY, @sprintf("%s_predictions.jld", catalog_label))
+jld_filename = joinpath(
+    OUTPUT_DIRECTORY,
+    @sprintf(
+        "celeste-%.4f-%.4f-%.4f-%.4f.jld",
+        minimum(results_df[:right_ascension_deg]),
+        maximum(results_df[:right_ascension_deg]),
+        minimum(results_df[:declination_deg]),
+        maximum(results_df[:declination_deg]),
+    )
+)
+println("Writing $(jld_filename)...")
 JLD.save(jld_filename, "results", results)
-AccuracyBenchmark.append_hash_to_file(jld_filename)
 
 csv_filename = joinpath(OUTPUT_DIRECTORY, @sprintf("%s_predictions.csv", catalog_label))
 AccuracyBenchmark.write_catalog(csv_filename, results_df)
