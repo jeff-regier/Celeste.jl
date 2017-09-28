ed67bcac51df331dec5a6838c11854e01f2a4c50
Jeffrey Regier <regier@gmail.com>
2017-09-27 20:50:11 -0700

    added a seperate script to filter out NA entries from the coadd catalog rather than doing it in generate_synthetic_field.jl

---
+++ benchmark/accuracy/filter_coadd_catalog_na.jl
@@ -0,0 +1,33 @@
+#!/usr/bin/env julia
+
+# This script takes a catalog in csv format, removes rows containing NA values
+# for flux and color, and writes out the result in csv format.
+# The original catalog (with NAs) is useful for initializing Celeste on coadd
+# images. The filtered catalog is useful for scoring Celeste on coadd images.
+# The filtered catalog is also useful for generating synthetic images based on
+# a real catalog, and then initializing and scoring Celeste on those synthetic
+# images.
+
+using DataFrames
+
+import Celeste.AccuracyBenchmark
+import Celeste.ArgumentParse
+
+parser = Celeste.ArgumentParse.ArgumentParser()
+ArgumentParse.add_argument(
+    parser,
+    "catalog_csv",
+    help="Unfiltered CSV catalog",
+)
+parsed_args = ArgumentParse.parse_args(parser, ARGS)
+
+catalog_data = AccuracyBenchmark.read_catalog(parsed_args["catalog_csv"])
+no_na_cols = :flux_r_nmgy, :color_ug, :color_gr, :color_ri, :color_iz
+for col in no_na_cols
+    catalog_data = catalog_data[.!isna.(catalog_data[col]), :]
+end
+
+catalog_label = splitext(parsed_args["catalog_csv"])[1]
+output_filename = @sprintf("%s_filtered.csv", catalog_label)
+AccuracyBenchmark.write_catalog(output_filename, catalog_data)
+
